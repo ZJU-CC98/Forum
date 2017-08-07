da016c1c4bc410c344ecab01a9351c368338c83c
Iris Sakura <sgjsakura@hotmail.com>
2017-08-07 16:21:17 +0800

    初始化项目

---
+++ CC98.Forum/CC98.Forum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CC98.Forum
+{
+	/// <summary>
+	/// 应用程序的主类型。
+	/// </summary>
+    public static class Program
+    {
+		/// <summary>
+		/// 应用程序的启动方法。
+		/// </summary>
+		/// <param name="args">应用程序的启动参数。</param>
+		[UsedImplicitly]
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ CC98.Forum/CC98.Forum/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CC98.Forum
+{
+	/// <summary>
+	/// 应用程序的启动类型。
+	/// </summary>
+	public class Startup
+	{
+		/// <summary>
+		/// 配置应用程序服务。
+		/// </summary>
+		/// <param name="services">应用程序的服务容器。</param>
+		[UsedImplicitly]
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
+
+		/// <summary>
+		/// 配置应用程序功能。
+		/// </summary>
+		/// <param name="app">应用程序对象。</param>
+		[UsedImplicitly]
+		public void Configure(IApplicationBuilder app)
+		{
+			// 允许静态文件请求。
+			app.UseStaticFiles();
+
+			// 对其它所有路径执行 URL 重写并返回 index 文件。
+			var rewriteOptions = new RewriteOptions().AddRewrite(@".*", "index.html", true);
+			app.UseRewriter(rewriteOptions);
+			app.UseStaticFiles();
+		}
+	}
+}
