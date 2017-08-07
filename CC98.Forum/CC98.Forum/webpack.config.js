0c94c251cd8d407bb6256264a8dae5148769453d
子珺 田 <tzj912016486@vip.qq.com>
2017-08-07 17:57:42 +0800

    初始配置

---
+++ CC98.Forum/CC98.Forum/webpack.config.js
@@ -0,0 +1,31 @@
+﻿var webpack = require("webpack");
+module.exports = () => {
+
+    var config = {
+        module: {
+            rules: [
+                { test: /\.tsx?$/, use: 'awesome-typescript-loader' }
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        entry: {
+            'script': './App.tsx',
+            vendor: ["react-router", "react-router-dom"]
+        },
+        output: {
+            filename: 'final.js',
+            path: __dirname + '/wwwroot'
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "vendor.final.js" })
+        ],
+        externals: {
+            "react": "React",
+            "react-dom":"ReactDOM"
+        }
+    };
+
+    return [config];
+};
