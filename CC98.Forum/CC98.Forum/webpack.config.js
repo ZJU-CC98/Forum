3f18d626f67ad69c38c69b364f784c6a1121cba5
Iris Sakura <sgjsakura@hotmail.com>
2017-08-08 15:55:41 +0800

    添加必要库引用和自动生成脚本

--- CC98.Forum/CC98.Forum/webpack.config.js
+++ CC98.Forum/CC98.Forum/webpack.config.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var webpack = require("webpack");
 var path = require("path");
 var UnminifiedWebpackPlugin = require("unminified-webpack-plugin");
-var TransferWebpackPlugin = require("transfer-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
 var config = {
     module: {
         rules: [
@@ -19,15 +20,22 @@ var config = {
         path: path.resolve(__dirname, 'wwwroot'),
         filename: 'main.min.js'
     },
-    externals: ['jquery', 'signalr', 'react', 'react-dom'],
+    externals: ['jquery', 'signalr', 'react', 'react-dom', 'react-router', 'react-router-dom', 'redux', 'react-redux'],
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-        }),
+        new webpack.optimize.UglifyJsPlugin(),
         new UnminifiedWebpackPlugin(),
-        new TransferWebpackPlugin([
-            { from: 'jquery', to: 'lib/jquery' }
-        ], path.join(__dirname, 'node_modules'))
+        new CleanWebpackPlugin(['wwwroot/*']),
+        new CopyWebpackPlugin([
+            { from: 'index.html', to: 'index.html' },
+            { from: 'node_modules/jquery/dist', to: 'lib/jquery' },
+            { from: 'node_modules/signalr', to: 'lib/signalr' },
+            { from: 'node_modules/react/dist', to: 'lib/react' },
+            { from: 'node_modules/react-dom/dist', to: 'lib/react-dom' },
+            { from: 'node_modules/react-router/umd', to: 'lib/react-router' },
+            { from: 'node_modules/react-router-dom/umd', to: 'lib/react-router-dom' },
+            { from: 'node_modules/redux/dist', to: 'lib/redux' },
+            { from: 'node_modules/react-redux/dist', to: 'lib/react-redux' }
+        ])
     ]
 };
 exports.default = config;
--- CC98.Forum/CC98.Forum/webpack.config.ts
+++ CC98.Forum/CC98.Forum/webpack.config.ts
@@ -1,8 +1,10 @@
 ﻿import * as webpack from 'webpack';
 import * as path from 'path';
 import * as UnminifiedWebpackPlugin from 'unminified-webpack-plugin';
-import * as TransferWebpackPlugin from 'transfer-webpack-plugin';
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
+import * as CleanWebpackPlugin from 'clean-webpack-plugin';
 
+// ReSharper disable once InconsistentNaming
 declare var __dirname;
 
 const config: webpack.Configuration = {
@@ -20,16 +22,23 @@ const config: webpack.Configuration = {
 		path: path.resolve(__dirname, 'wwwroot'),
 		filename: 'main.min.js'
 	},
-	externals: ['jquery', 'signalr', 'react', 'react-dom'],
+	externals: ['jquery', 'signalr', 'react', 'react-dom', 'react-router', 'react-router-dom', 'redux', 'react-redux'],
 	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true
-		}),
-		new UnminifiedWebpackPlugin(),
-		new TransferWebpackPlugin([
-			{ from: 'jquery', to: 'lib/jquery' }
+		new webpack.optimize.UglifyJsPlugin(), // 简化 JS
+		new UnminifiedWebpackPlugin(), // 提供调试用 JS 完整版
+		new CleanWebpackPlugin(['wwwroot/*']), // 发布之前清理 wwwroot
+		new CopyWebpackPlugin([// 将 node 库复制到发布目录
+			{ from: 'index.html', to: 'index.html' }, // 复制 index.html
+			{ from: 'node_modules/jquery/dist', to: 'lib/jquery' },
+			{ from: 'node_modules/signalr', to: 'lib/signalr' },
+			{ from: 'node_modules/react/dist', to: 'lib/react' },
+			{ from: 'node_modules/react-dom/dist', to: 'lib/react-dom' },
+			{ from: 'node_modules/react-router/umd', to: 'lib/react-router' },
+			{ from: 'node_modules/react-router-dom/umd', to: 'lib/react-router-dom' },
+			{ from: 'node_modules/redux/dist', to: 'lib/redux' },
+			{ from: 'node_modules/react-redux/dist', to: 'lib/react-redux' }
 
-		], path.join(__dirname,  'node_modules'))
+		])
 	]
 };
 
