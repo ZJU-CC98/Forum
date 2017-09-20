0239c24b8fde0e7febeedf9cc77a21cde3dbd1cc
Iris Sakura <sgjsakura@hotmail.com>
2017-09-20 14:27:37 +0800

    添加注释

--- CC98.Forum/CC98.Forum/Components/UbbContainer.tsx
+++ CC98.Forum/CC98.Forum/Components/UbbContainer.tsx
@@ -5,12 +5,27 @@
 import * as React from 'react';
 import * as Ubb from '../Ubb/UbbCodeExtension';
 
+/**
+ * 定义 UBBContainer 组件需要使用的属性。
+ */
 export class UbbContainerProps {
+	/**
+	 * UBB 容器需要解析的 UBB 代码字符串。
+	 */
 	code: string;
+	/**
+	 * UBB 容器使用的 UBB 引擎。引擎可在多次解析时重复使用，从而实现对整个文档的跨容器状态维护（如对特定内容进行统一计数等）。
+	 */
 	engine?: Ubb.UbbCodeEngine;
+	/**
+	 * UBB 解析时使用的相关选项。
+	 */
 	options?: Ubb.UbbCodeOptions;
 }
 
+/**
+ * 提供用于解析 UBB 的核心组件。
+ */
 export class UbbContainer extends React.Component<UbbContainerProps, {}> {
 
 	render() {
@@ -22,7 +37,7 @@ export class UbbContainer extends React.Component<UbbContainerProps, {}> {
 
 		const ubbHtml = engine.exec(this.props.code || '', options);
 
-		//打开回车与空格
+		//打开回车与空格00
 		const style = {
 			whiteSpace: 'pre-line'
 
