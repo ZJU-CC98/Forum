ae66ccde839205f071af845ad0cb7eea1446666d
adddna <zzaadddna@live.com>
2017-09-23 14:31:16 +0800

    修改更改

--- CC98.Forum/CC98.Forum/Components/Footer.tsx
+++ CC98.Forum/CC98.Forum/Components/Footer.tsx
@@ -2,8 +2,8 @@
 import { AppState } from '../States/AppState';
 import * as $ from 'jquery';
 
-export class Footer extends React.Component<{}, AppState> {   //顶部条的下拉菜单组件
-    render() {
+export class Footer extends React.Component<{}, AppState> {   //顶部条的下拉菜单组件 
+    render() {     
         return <div className="footer">
             <div className="footerRow">友情链接    |    浙江大学    |    浙江大学计算机学院    |    浙江大学体育学院    |    浙江大学文创学院    |    浙江大学挖掘机学院</div>
             <div className="footerRow">浙江大学挖掘机学院    |    浙江大学挖掘机学院    |    浙江大学挖掘机学院    |    浙江大学挖掘机学院    |    浙江大学挖掘机学院    |    浙江大学挖掘机学院    |    浙江大学挖掘机学院</div>
