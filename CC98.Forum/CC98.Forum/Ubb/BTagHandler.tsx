ea43e35ee11cc19d9c08876eea2b96d00ae6c1bb
adddna <zzaadddna@live.com>
2017-11-16 20:42:48 +0800

    修改b标签的解析

--- CC98.Forum/CC98.Forum/Ubb/BTagHandler.tsx
+++ CC98.Forum/CC98.Forum/Ubb/BTagHandler.tsx
@@ -9,17 +9,20 @@ import * as Ubb from './Core';
  * 处理 [b] 标签的处理器。
  */
 export class BTagHandler extends Ubb.RecursiveTagHandler {
-	get supportedTagNames(): string {
-		return 'b';
-	}
+    get supportedTagNames(): string {
+        return 'b';
+    }
 
-	execCore(innerContent: React.ReactNode, tagData: Ubb.UbbTagData, context: Ubb.UbbCodeContext): React.ReactNode {
-
+    execCore(innerContent: React.ReactNode, tagData: Ubb.UbbTagData, context: Ubb.UbbCodeContext): React.ReactNode {
+        /*
 		// HTML5 标准建议不再使用 b 标签
 		if (context.options.compatibility === Ubb.UbbCompatiblityMode.EnforceMorden) {
 			return <strong>{innerContent}</strong>;
 		} else {
 			return <b>{innerContent}</b>;
 		}
-	}
+        */
+
+        return <span style={{ fontWeight: "bold" }}>{innerContent}</span>;
+    }
 }
