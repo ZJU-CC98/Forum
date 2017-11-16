1c67d6e1afb56f0f9959a77b5d2f2ea5be7bf08c
AsukaSong <yansongdong@gmail.com>
2017-11-16 14:17:11 +0800

    用户中心-我的粉丝

--- CC98.Forum/CC98.Forum/Components/UserCenterMyFans.tsx
+++ CC98.Forum/CC98.Forum/Components/UserCenterMyFans.tsx
@@ -6,16 +6,78 @@ import * as React from 'react';
 import { UserFanInfo } from '../States/AppState';
 import { UserCenterMyFollowingsUser } from './UserCenterMyFollowingsUser';
 
+import * as Utility from '../Utility';
+
 //用户中心我的粉丝组件
 export class UserCenterMyFans extends React.Component<null, UserCenterMyFansState> {
     constructor(props) {
         super(props);
         this.state = {
-            //临时填充信息
-            userFans: [userFanInfo, userFanInfo, userFanInfo]
+            userFans: []
         };
     }
+
+    async componentDidMount() {
+        // bugtest 550524
+        // 董松松松 569380
+
+        const token = window.localStorage.accessToken.slice(4);
+
+        let url = 'http://apitest.niconi.cc/user/follow/fan?from=0&size=10';
+
+        let res = await fetch(url, {
+            headers: {
+                'Authorization': token
+            }
+        });
+
+        let data = await res.json();
+        
+
+        //没有粉丝
+
+        if (!data || !data.length) {
+            return false;
+        }
+
+        let fans: UserFanInfo[] = [];
+        let userFanInfo = new UserFanInfo();
+        let i = data.length, data2;
+
+        while (i--) {
+            let userid = data[i];
+
+            url = `http://apitest.niconi.cc/user/${userid}`;
+            res = await fetch(url);
+            data2 = await res.json();
+
+            userFanInfo.name = data2.name;
+            userFanInfo.avatarImgURL = data2.portraitUrl;
+            userFanInfo.posts = data2.postCount;
+
+            url = `http://apitest.niconi.cc/user/follow/fancount?userid=${userid}`
+            res = await fetch(url);
+            data2 = await res.json();
+
+            userFanInfo.fans = data2;
+
+            fans.push(userFanInfo);
+        }
+
+        console.log(fans);
+        
+        this.setState({
+            userFans: fans
+        });
+    }
+
     render() {
+        if (this.state.userFans.length === 0) {
+            return (<div className="user-center-myfans">
+                没有粉丝
+            </div>);
+        }
+
         //state转换为JSX
         const userFans = this.state.userFans.map((item) => (<UserCenterMyFollowingsUser userFanInfo={item} />));
         //添加分隔线
@@ -32,10 +94,3 @@ export class UserCenterMyFans extends React.Component<null, UserCenterMyFansStat
 interface UserCenterMyFansState {
     userFans: UserFanInfo[];
 }
-
-//临时填充信息
-let userFanInfo = new UserFanInfo();
-userFanInfo.avatarImgURL = '../img/001.jpg';
-userFanInfo.fans = 666;
-userFanInfo.posts = 233;
-userFanInfo.name = '董松松松';
