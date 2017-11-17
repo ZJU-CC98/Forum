4698fe4b6999a2da8d5d11d8fdba8113e993debf
adddna <zzaadddna@live.com>
2017-11-17 16:31:37 +0800

    添加left center right 标签

---
+++ CC98.Forum/CC98.Forum/Ubb/CenterTagHandler.tsx
@@ -0,0 +1,23 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+import * as React from 'react';
+import * as Ubb from './Core';
+
+/**
+ * 处理 [center] 标签的处理器。
+ */
+export class CenterTagHandler extends Ubb.RecursiveTagHandler {
+
+    get supportedTagNames(): string { return 'center' };
+
+    execCore(innerContent: React.ReactNode, tagData: Ubb.UbbTagData, context: Ubb.UbbCodeContext): React.ReactNode {
+
+        const style = {
+            textAlign: 'center'
+        };
+
+        return <span style={style}><div>{innerContent}</div></span>;
+    }
+}
