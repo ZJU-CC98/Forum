8e7146f93d4c571ba3cf7bd0971f891518ec649b
AsukaSong <yansongdong@gmail.com>
2017-11-24 00:49:55 +0800

    用户中心

--- CC98.Forum/CC98.Forum/Components/UserCenter/UserCenterConfigAvatar.tsx
+++ CC98.Forum/CC98.Forum/Components/UserCenter/UserCenterConfigAvatar.tsx
@@ -10,7 +10,7 @@ export class UserCenterConfigAvatar extends React.Component<null, UserCenterConf
     myIMG: HTMLImageElement;
     selector: HTMLDivElement;
     resize: HTMLSpanElement;
-
+    newAvatar: HTMLCanvasElement;
     constructor(props) {
         super(props);
         const userInfo = Utility.getLocalStorage('userInfo');
@@ -28,11 +28,11 @@ export class UserCenterConfigAvatar extends React.Component<null, UserCenterConf
         this.handleIMGLoad = this.handleIMGLoad.bind(this);
         this.handleSelectorMove = this.handleSelectorMove.bind(this);
         this.handleResizeMove = this.handleResizeMove.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     handleChange(e) {
         let file = e.target.files[0];
-
         if (!file.type.match('image.*')) {
             this.setState({
                 info: '请选择图片文件',
@@ -128,6 +128,36 @@ export class UserCenterConfigAvatar extends React.Component<null, UserCenterConf
                 break;
         }
     }
+
+    handleSubmit() {
+        let canvas = this.newAvatar;
+        let ctx = canvas.getContext('2d');
+        const x = this.state.selectorLeft-20,
+            y = this.state.selectorTop-20,
+            width = this.state.selectorWidth;
+        canvas.width = width;
+        canvas.height = width;
+        ctx.drawImage(this.myIMG, x, y, width, width, 0, 0, width, width);
+        canvas.toBlob(async (result) => {
+            let file = new File([result], '头像.jpg', { type: 'image/jpeg', lastModified: Date.now() });
+            const avatar = await Utility.uploadFile(file);
+            const token = Utility.getLocalStorage('accessToken');
+            const url = 'http://apitest.niconi.cc/user/portrait';
+            let myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json');
+            myHeaders.append('Authorization', token);
+            let data = `http://apitest.niconi.cc${avatar.content}`;
+            let res = await fetch(url, {
+                method: 'PUT',
+                headers: myHeaders,
+                body: JSON.stringify(data)
+            });
+            this.setState({
+                info: '修改成功'
+            });
+        },'image/jpeg',0.75);
+    }
+
     handleResizeMove(event) {
         switch (event.type) {
             case 'mousedown':
@@ -137,6 +167,9 @@ export class UserCenterConfigAvatar extends React.Component<null, UserCenterConf
             case 'mousemove':
                 this.diffY = event.clientX - event.target.offsetLeft;
                 if (this.dragging !== null) {
+                    console.log(this.state.selectorWidth);
+                    console.log(this.state.selectorTop);
+                    console.log(`${this.state.selectorWidth + this.state.selectorTop}px`);
                     this.setState((prevState) => {
                         let num = prevState.selectorWidth + this.diffY - this.diffX;
                         if (!isNaN(num)) {
@@ -173,18 +206,20 @@ export class UserCenterConfigAvatar extends React.Component<null, UserCenterConf
                             <input onChange={this.handleChange} id="uploadAvatar" type="file" style={style} />
                             <label htmlFor="uploadAvatar"><p>选择本地图片</p></label>
                             <p>{this.state.info}</p>
+                            <button type="button" style={this.state.isShown ? {} : style} onClick={this.handleSubmit}>提交</button>
                         </div>
                     </div>
                     <div className="user-center-config-avatar-preview" style={this.state.isShown ? { opacity: 1, marginTop: '2rem' } : { zIndex: -1 }}>
                         <hr />
                         <div style={{ position: 'absolute', width: '824px', overflow: 'hidden' }}>
+                            <canvas id="newAvatar" style={style} ref={(a) => { this.newAvatar = a;}}></canvas>
                             <canvas ref={(canvas) => { this.myCanvas = canvas }} style={{ position: 'relative'}} />
                             <div id="cover"></div>
                             <div className="imgdata" ref={(div) => { this.selector = div; }} style={{ width: `${this.state.selectorWidth}px`, height: `${this.state.selectorWidth}px`, borderRadius: `${this.state.selectorWidth / 2}px`, top: `${this.state.selectorTop}px`, left: `${this.state.selectorLeft}px` }}>
                                 <img src={this.state.avatarURL} style={{ position: 'relative', top: `${20-this.state.selectorTop}px`, left: `${20-this.state.selectorLeft}px` }} />
                             </div>
                             <div id="selector" ref={(div) => { this.selector = div; }} style={{ width: `${this.state.selectorWidth}px`, height: `${this.state.selectorWidth}px`, borderRadius: `${this.state.selectorWidth / 2}px`, top: `${this.state.selectorTop}px`, left: `${this.state.selectorLeft}px` }}></div>
-                            <span id="resize" ref={(span) => { this.resize = span; }} style={{ top: `${this.state.selectorTop + this.state.selectorWidth}px`, left: `${this.state.selectorLeft + this.state.selectorWidth}px` }}></span>
+                            <span id="resize" ref={(span) => { this.resize = span; }} style={{ top: `${this.state.selectorWidth + this.state.selectorTop}px`, left: `${this.state.selectorWidth + this.state.selectorLeft}px` }}></span>
                         </div>
                         <img ref={(img) => { this.myIMG = img; }} onLoad={this.handleIMGLoad} style={style} src={this.state.avatarURL} />
                     </div>
--- CC98.Forum/CC98.Forum/wwwroot/scripts/main.js
+++ CC98.Forum/CC98.Forum/wwwroot/scripts/main.js
@@ -14746,6 +14746,41 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 var Utility = __webpack_require__(1);
@@ -14767,6 +14802,7 @@ var UserCenterConfigAvatar = /** @class */ (function (_super) {
         _this.handleIMGLoad = _this.handleIMGLoad.bind(_this);
         _this.handleSelectorMove = _this.handleSelectorMove.bind(_this);
         _this.handleResizeMove = _this.handleResizeMove.bind(_this);
+        _this.handleSubmit = _this.handleSubmit.bind(_this);
         return _this;
     }
     UserCenterConfigAvatar.prototype.handleChange = function (e) {
@@ -14863,6 +14899,44 @@ var UserCenterConfigAvatar = /** @class */ (function (_super) {
                 break;
         }
     };
+    UserCenterConfigAvatar.prototype.handleSubmit = function () {
+        var _this = this;
+        var canvas = this.newAvatar;
+        var ctx = canvas.getContext('2d');
+        var x = this.state.selectorLeft - 20, y = this.state.selectorTop - 20, width = this.state.selectorWidth;
+        canvas.width = width;
+        canvas.height = width;
+        ctx.drawImage(this.myIMG, x, y, width, width, 0, 0, width, width);
+        canvas.toBlob(function (result) { return __awaiter(_this, void 0, void 0, function () {
+            var file, avatar, token, url, myHeaders, data, res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        file = new File([result], '头像.jpg', { type: 'image/jpeg', lastModified: Date.now() });
+                        return [4 /*yield*/, Utility.uploadFile(file)];
+                    case 1:
+                        avatar = _a.sent();
+                        token = Utility.getLocalStorage('accessToken');
+                        url = 'http://apitest.niconi.cc/user/portrait';
+                        myHeaders = new Headers();
+                        myHeaders.append('Content-Type', 'application/json');
+                        myHeaders.append('Authorization', token);
+                        data = "http://apitest.niconi.cc" + avatar.content;
+                        return [4 /*yield*/, fetch(url, {
+                                method: 'PUT',
+                                headers: myHeaders,
+                                body: JSON.stringify(data)
+                            })];
+                    case 2:
+                        res = _a.sent();
+                        this.setState({
+                            info: '修改成功'
+                        });
+                        return [2 /*return*/];
+                }
+            });
+        }); }, 'image/jpeg', 0.75);
+    };
     UserCenterConfigAvatar.prototype.handleResizeMove = function (event) {
         var _this = this;
         switch (event.type) {
@@ -14873,6 +14947,9 @@ var UserCenterConfigAvatar = /** @class */ (function (_super) {
             case 'mousemove':
                 this.diffY = event.clientX - event.target.offsetLeft;
                 if (this.dragging !== null) {
+                    console.log(this.state.selectorWidth);
+                    console.log(this.state.selectorTop);
+                    console.log(this.state.selectorWidth + this.state.selectorTop + "px");
                     this.setState(function (prevState) {
                         var num = prevState.selectorWidth + _this.diffY - _this.diffX;
                         if (!isNaN(num)) {
@@ -14913,16 +14990,18 @@ var UserCenterConfigAvatar = /** @class */ (function (_super) {
                         React.createElement("input", { onChange: this.handleChange, id: "uploadAvatar", type: "file", style: style }),
                         React.createElement("label", { htmlFor: "uploadAvatar" },
                             React.createElement("p", null, "\u9009\u62E9\u672C\u5730\u56FE\u7247")),
-                        React.createElement("p", null, this.state.info))),
+                        React.createElement("p", null, this.state.info),
+                        React.createElement("button", { type: "button", style: this.state.isShown ? {} : style, onClick: this.handleSubmit }, "\u63D0\u4EA4"))),
                 React.createElement("div", { className: "user-center-config-avatar-preview", style: this.state.isShown ? { opacity: 1, marginTop: '2rem' } : { zIndex: -1 } },
                     React.createElement("hr", null),
                     React.createElement("div", { style: { position: 'absolute', width: '824px', overflow: 'hidden' } },
+                        React.createElement("canvas", { id: "newAvatar", style: style, ref: function (a) { _this.newAvatar = a; } }),
                         React.createElement("canvas", { ref: function (canvas) { _this.myCanvas = canvas; }, style: { position: 'relative' } }),
                         React.createElement("div", { id: "cover" }),
                         React.createElement("div", { className: "imgdata", ref: function (div) { _this.selector = div; }, style: { width: this.state.selectorWidth + "px", height: this.state.selectorWidth + "px", borderRadius: this.state.selectorWidth / 2 + "px", top: this.state.selectorTop + "px", left: this.state.selectorLeft + "px" } },
                             React.createElement("img", { src: this.state.avatarURL, style: { position: 'relative', top: 20 - this.state.selectorTop + "px", left: 20 - this.state.selectorLeft + "px" } })),
                         React.createElement("div", { id: "selector", ref: function (div) { _this.selector = div; }, style: { width: this.state.selectorWidth + "px", height: this.state.selectorWidth + "px", borderRadius: this.state.selectorWidth / 2 + "px", top: this.state.selectorTop + "px", left: this.state.selectorLeft + "px" } }),
-                        React.createElement("span", { id: "resize", ref: function (span) { _this.resize = span; }, style: { top: this.state.selectorTop + this.state.selectorWidth + "px", left: this.state.selectorLeft + this.state.selectorWidth + "px" } })),
+                        React.createElement("span", { id: "resize", ref: function (span) { _this.resize = span; }, style: { top: this.state.selectorWidth + this.state.selectorTop + "px", left: this.state.selectorWidth + this.state.selectorLeft + "px" } })),
                     React.createElement("img", { ref: function (img) { _this.myIMG = img; }, onLoad: this.handleIMGLoad, style: style, src: this.state.avatarURL })),
                 React.createElement("div", { style: { width: '100%', height: this.state.divheight, transitionDuration: '.5s' } }))));
     };
