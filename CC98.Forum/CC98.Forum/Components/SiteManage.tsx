71c547ebbcc9cfcdd498315f9f6a8d5fb41e982e
AsukaSong <yansongdong@gmail.com>
2017-12-12 20:08:41 +0800

    用户中心的一些修改

---
+++ CC98.Forum/CC98.Forum/Components/SiteManage.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from 'react';
+
+export class SiteManage extends React.Component {
+
+    render() {
+        return (<div>
+            <p>全站管理</p>
+            <div>
+
+            </div>
+        </div>);
+    }
+}
