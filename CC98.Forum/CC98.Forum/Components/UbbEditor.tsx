8fc83690a3cc40ca8160cd96a067bfa5ff0546d6
AsukaSong <yansongdong@gmail.com>
2017-12-16 16:10:44 +0800

    UBB编辑器的一些修改

--- CC98.Forum/CC98.Forum/Components/UbbEditor.tsx
+++ CC98.Forum/CC98.Forum/Components/UbbEditor.tsx
@@ -112,6 +112,7 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
             value: ''
         };
         this.option = props.option || new UbbEditorOption();
+        this.clearAllShown = this.clearAllShown.bind(this);
         this.handleExtendValueChange = this.handleExtendValueChange.bind(this);
         this.handleTextareaChange = this.handleTextareaChange.bind(this);
         this.handleTextareaBlur = this.handleTextareaBlur.bind(this);
@@ -150,49 +151,44 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
         this.handleButtonClick(this.state.extendTagName, `http://apitest.niconi.cc${res.content}`);
     }
 
-    handleUndo(redo: boolean) {
-        if (!redo) {
-            this.setState((prevState) => {
-                if (this.valueStack.length === 1) {
-                    return {value: ''}
-                }
-                let prevValue = this.valueStack.pop();
-                this.redoStack.push(prevValue);
-                if (prevValue !== undefined) {
-                    if (prevValue === prevState.value) {
-                        if (this.valueStack.length > 0) {
-                            prevValue = this.valueStack[this.valueStack.length - 1];
-                        } else {
-                            prevValue = '';
-                        }
-                    }
-                    this.props.update(prevValue);
-                    return { value: prevValue || '' };
-                }
-            });
-        } else {
-            this.setState((prevState) => {
-                let prevValue = this.redoStack.pop();
-                if (prevValue !== undefined) {
-                    this.valueStack.push(prevValue);
-                    this.props.update(prevValue);
-                    return { value: prevValue };
-                }                
-            });
-        }
+    handleUndo() {
+        this.setState((prevState) => {
+            if (this.valueStack.length === 1) {
+                return {value: ''}
+            }
+            let prevValue = this.valueStack.pop();
+            this.redoStack.push(prevValue);
+            prevValue = this.valueStack[this.valueStack.length - 1];
+            this.props.update(prevValue);
+            return { value: prevValue };
+        });
+    }
+
+    handleRedo() {
+        this.setState((prevState) => {
+            let prevValue: string;
+            if (prevValue = this.redoStack.pop()) {
+                this.valueStack.push(prevValue);
+                this.props.update(prevValue);
+                return { value: prevValue };
+            }
+        });
     }
 
     handleButtonClick(name: string, value = '') {
         const shouldReplaceSelection = ['video', 'audio', 'img', 'upload'].indexOf(name) !== -1;
-
+        const hasDefaultSelection = ['url'].indexOf(name) !== -1;
         this.setState((prevState: UbbEditorState) => {
             let before = this.state.value.slice(0, prevState.selectionStart),
                 selected = this.state.value.slice(prevState.selectionStart, prevState.selectionEnd),
                 after = this.state.value.slice(prevState.selectionEnd, this.state.value.length);
+            console.log(selected);
             if (shouldReplaceSelection) {
                 selected = `[${name}]${value}[/${name}]`;
-            } else {
+            } else if (hasDefaultSelection) {
                 selected = `[${name}${value ? `=${value}` : ''}]${selected || value}[/${name}]`;
+            } else {
+                selected = `[${name}${value ? `=${value}` : ''}]${selected}[/${name}]`;
             }
             this.props.update(before + selected + after);
             this.valueStack.push(before + selected + after);
@@ -222,6 +218,14 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
         });
     }
 
+    clearAllShown() {
+        this.setState({
+            emojiIsShown: false,
+            extendTagName: '',
+            extendValue: ''
+        });
+    }
+
     componentWillReceiveProps(nextProps) {
         if (this.valueStack[this.valueStack.length - 1] !== nextProps.value) {
             this.valueStack.push(nextProps.value);
@@ -233,8 +237,6 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
     }
 
     componentDidUpdate() {
-        console.log(this.valueStack);
-        console.log(this.redoStack);
         if (this.state.clicked && !this.state.isPreviewing) {
             this.content.focus();
             this.content.setSelectionRange(this.state.selectionStart, this.state.selectionEnd);
@@ -244,10 +246,38 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
         }
     }
 
+    componentDidMount() {
+        window.addEventListener('click', this.clearAllShown);
+        ($("#color") as any).spectrum({
+            color: "#000",
+            change: (color) => {
+                this.handleButtonClick('color', color.toHexString());
+                ($("#color") as any).spectrum('set', '#000000');
+            },
+            showPalette: true,
+            palette: [
+                ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"],
+                ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"]
+            ],
+            replacerClassName: 'ubb-color-picker',
+            hideAfterPaletteSelect: true
+        });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('click', this.clearAllShown);
+    }
+
     render() {
+
         const height = this.option.height;
         const size = ['', 1, 2, 3, 4, 5, 6, 7];
-        const color = ['颜色', 'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
         const mohjong = {
             animal: ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011', '012', '013', '014', '015', '016'].map((item) => (<img
                 src={`/images/mahjong/animal2017/${item}.png`}
@@ -315,44 +345,39 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
                             <p className="fa-text-height"></p>
                             <select
                                 onChange={(e) => { this.handleButtonClick('size', e.target.value); (e.target.value as any) = 0; }}
-                                onClick={() => { this.setState({ extendTagName: '', extendValue: '', emojiIsShown: false}); }}
+                                onClick={() => { this.clearAllShown(); }}
                                 value={0}
                             >
                                 {size.map((value, index) => (<option value={index} disabled={index === 0} style={{ display: index === 0 ? 'none' : '' }}>{value}</option>))}
                             </select>
                             <p className="fa-eyedropper"></p>
-                            <select
-                                onChange={(e) => { this.handleButtonClick('color', e.target.value); (e.target.value as any) = "颜色"; }}
-                                onClick={() => { this.setState({ extendTagName: '', extendValue: '', emojiIsShown: false}); }}
-                                value={"颜色"}
-                            >
-                                {color.map((value, index) => (<option value={value} disabled={index === 0} style={{ backgroundColor: value, display: index === 0 ? 'none' : '' }}></option>))}
-                            </select>
+                            <input id="color" />
                         </div>
                         <div className="editor-buttons-extends">
                             <button
                                 className="fa-smile-o"
                                 type="button"
                                 title="插入表情"
-                                onClick={() => {
-                                this.setState((prev) => ({
-                                    emojiIsShown: !prev.emojiIsShown,
-                                    extendTagName: '',
-                                    extendValue: ''
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    this.setState((prev) => ({
+                                        emojiIsShown: !prev.emojiIsShown,
+                                        extendTagName: '',
+                                        extendValue: ''
                                     }));
                                 }}
                             ></button>
-                            <button className="fa-link" type="button" title="插入url" onClick={() => { this.handleExtendButtonClick('url'); }}></button>
-                            <button className="fa-picture-o" type="button" title="插入图片" onClick={() => { this.handleExtendButtonClick('img'); }}></button>
-                            <button className="fa-film" type="button" title="插入视频" onClick={() => { this.handleExtendButtonClick('video'); }}></button>
-                            <button className="fa-music" type="button" title="插入音频" onClick={() => { this.handleExtendButtonClick('audio'); }}></button>
-                            <label className="fa-file" htmlFor="upload" title="上传文件" onClick={() => { this.setState({ extendTagName: 'upload' }); }} ></label>
+                            <button className="fa-link" type="button" title="插入url" onClick={(e) => { e.stopPropagation();this.handleExtendButtonClick('url'); }}></button>
+                            <button className="fa-picture-o" type="button" title="插入图片" onClick={(e) => { e.stopPropagation();this.handleExtendButtonClick('img'); }}></button>
+                            <button className="fa-film" type="button" title="插入视频" onClick={(e) => { e.stopPropagation();this.handleExtendButtonClick('video'); }}></button>
+                            <button className="fa-music" type="button" title="插入音频" onClick={(e) => { e.stopPropagation(); this.handleExtendButtonClick('audio'); }}></button>
+                            <label className="fa-file" htmlFor="upload" title="上传文件" onClick={(e) => { e.stopPropagation(); this.setState({ extendTagName: 'upload' }); }} ></label>
                         </div>
                     </div>
                     <div style={{ flexGrow: 1 }}></div>
-                    <button className="fa-undo" type="button" title="撤销" onClick={() => { this.handleUndo(false); }}></button>
-                    <button className="fa-repeat" type="button" title="重做" onClick={() => { this.handleUndo(true); }}></button>
-                    <button type="button" title="切换预览" onClick={() => { this.setState((prev) => ({ isPreviewing: !prev.isPreviewing, clicked: true, extendTagName: '', extendValue: '', emojiIsShown: false})); }} className="fa-window-maximize"></button>
+                    <button className="fa-undo" type="button" title="撤销" onClick={() => { this.handleUndo(); }}></button>
+                    <button className="fa-repeat" type="button" title="重做" onClick={() => { this.handleRedo(); }}></button>
+                    <button type="button" title="切换预览" onClick={() => { this.setState((prev) => ({ isPreviewing: !prev.isPreviewing, clicked: true})); }} className="fa-window-maximize"></button>
                 </div>
                 <div className="ubb-extend" style={{ height: this.state.extendTagName && this.state.extendTagName!== 'upload' ? '2rem' : '0rem' }}>
                     <input
@@ -360,16 +385,18 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
                         placeholder="在此输入地址"
                         value={this.state.extendValue}
                         onChange={(e) => { this.handleExtendValueChange(e.target.value); }}
+                        onClick={(e) => { e.stopPropagation();}}
                         ref={(it) => { this.input = it; }}
                     />
-                    {this.state.extendTagName === 'img' ? <label className="fa-upload" htmlFor="upload" title="上传本地图片"></label> : null}
-                    <button className="fa-check" type="button" onClick={() => { this.handleButtonClick(this.state.extendTagName, this.state.extendValue) }}></button>
-                    <button className="fa-remove" type="button" onClick={() => { this.setState({ extendTagName: '', clicked: true}); }}></button>
+                    {this.state.extendTagName === 'img' ? <label onClick={(e) => { e.stopPropagation(); }} className="fa-upload" htmlFor="upload" title="上传本地图片"></label> : null}
+                    <button className="fa-check" type="button" onClick={(e) => { e.stopPropagation(); this.handleButtonClick(this.state.extendTagName, this.state.extendValue) }}></button>
+                    <button className="fa-remove" type="button" onClick={() => { this.setState({ clicked: true}); }}></button>
                     <input
                         type="file"
                         id="upload"
                         accept={this.state.extendTagName === 'img' ? "image/*" : ""}
                         style={{ display: 'none' }}
+                        onClick={(e) => { e.stopPropagation(); }}
                         onChange={(e) => {
                             if (e.target.files[0]) {
                                 this.handleUpload(e.target.files[0]);
@@ -377,6 +404,7 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
                             }
                         }}
                     />
+                    
                 </div>
                 <div className="ubb-content">
                     {!this.state.isPreviewing ? (
@@ -387,7 +415,7 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
                                 this.redoStack = [];
                             }}
                             onFocus={() => {
-                                this.setState({ extendTagName: '', extendValue: '', emojiIsShown: false });
+                                this.clearAllShown();
                             }}
                             onBlur={(e) => {
                                 let target: any = e.target;
@@ -396,12 +424,11 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
                             onKeyDown={(e) => {
                                 if (e.ctrlKey && e.key === 'z') {
                                     e.preventDefault();
-                                    this.handleUndo(false);
+                                    this.handleUndo();
                                 } else if (e.ctrlKey && e.key === 'y') {
                                     e.preventDefault();
-                                    this.handleUndo(true);
+                                    this.handleRedo();
                                 }
-                                
                             }}
                             ref={(textarea) => {
                                 this.content = textarea;
@@ -415,9 +442,9 @@ export class UbbEditor extends React.Component<UbbEditorProps, UbbEditorState> {
                     style={this.state.emojiIsShown ? { height: '22rem', borderWidth: '1px', top: `-${height + 4}rem` } : { height: '0rem', top: `-${height + 4}rem` }}
                 >
                     <div className="ubb-emoji-buttons">
-                        <button type="button" className={this.state.emojiType === 'ac' ? 'ubb-emoji-button-active' : 'ubb-emoji-button'} onClick={() => { this.setState({ emojiType: 'ac' }); }}>AC娘</button>
-                        <button type="button" className={this.state.emojiType === 'mj' ? 'ubb-emoji-button-active' : 'ubb-emoji-button'} onClick={() => { this.setState({ emojiType: 'mj' }); }}>麻将脸</button>
-                        <button type="button" className={this.state.emojiType === 'em' ? 'ubb-emoji-button-active' : 'ubb-emoji-button'} onClick={() => { this.setState({ emojiType: 'em' }); }}>经典</button>
+                        <button type="button" className={this.state.emojiType === 'ac' ? 'ubb-emoji-button-active' : 'ubb-emoji-button'} onClick={(e) => { e.stopPropagation(); this.setState({ emojiType: 'ac' }); }}>AC娘</button>
+                        <button type="button" className={this.state.emojiType === 'mj' ? 'ubb-emoji-button-active' : 'ubb-emoji-button'} onClick={(e) => { e.stopPropagation(); this.setState({ emojiType: 'mj' }); }}>麻将脸</button>
+                        <button type="button" className={this.state.emojiType === 'em' ? 'ubb-emoji-button-active' : 'ubb-emoji-button'} onClick={(e) => { e.stopPropagation(); this.setState({ emojiType: 'em' }); }}>经典</button>
                     </div>
                     <div className={`ubb-emoji-content ubb-emoji-content-${this.state.emojiType}`}>
                         {emoji[this.state.emojiType]}
