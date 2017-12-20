f1931c8dff3703604e16e165da6c4ebbfc9a1e68
明一gg <zhoulm404@gmail.com>
2017-12-20 23:09:49 +0800

    bug修复

--- CC98.Forum/CC98.Forum/Components/Header.tsx
+++ CC98.Forum/CC98.Forum/Components/Header.tsx
@@ -198,7 +198,7 @@ export class SearchBeforeConnent extends React.Component<{history}, AppState> { 
         const searchBoxLi = searchBoxSub.find('li');
 
         //查看当前是全站还是某版，如果是某版就查询到某版id
-        let url1 = location.href.match(/\/topic\/(\S+)\/+?/);
+        let url1 = location.href.match(/\/topic\/(\S+)\/?/);
         let url2 = location.href.match(/\/list\/(\S+)\/?/);
         let url3 = location.href.match(/\/(search)/);
         let boardId = 0;
@@ -333,16 +333,14 @@ export class SearchBeforeConnent extends React.Component<{history}, AppState> { 
 
     render() {
         //查看当前是全站还是某版
-        let url1 = location.href.match(/\/topic\/(\S+)\/+?/);
+        let url1 = location.href.match(/\/topic\/(\S+)\/?/);
         let url2 = location.href.match(/\/list\/(\S+)\/?/);
         let url3 = location.href.match(/\/(search)/);
         let flag = 1;
         if (url1) {
-          
             flag = 0;
         }
         else if (url2) {
-         
             flag = 0;
         }
         else if (url3) {
