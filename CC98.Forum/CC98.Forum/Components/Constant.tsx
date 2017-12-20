2352e8837e4df8f88c2938d37ddfb8d63a90d8b6
Iris Sakura <sgjsakura@hotmail.com>
2017-12-20 14:20:32 +0800

    Api Update

--- CC98.Forum/CC98.Forum/Components/Constant.tsx
+++ CC98.Forum/CC98.Forum/Components/Constant.tsx
@@ -1,3 +1,54 @@
-﻿export module Constants {
-    export var testEditor;
+﻿/**
+* 系统级别配置对象。
+*/
+export interface IConfig {
+	/**
+	 * API 调用基础路径。
+	 */
+	apiUrl: string;
+	/**
+	 * 图片上传路径。
+	 */
+	imageUploadUrl: string;
 }
+
+/**
+ * 提供服务器端全局数据。
+ */
+export module Constants {
+
+	export var testEditor;
+
+	/**
+	 * 应用程序全局设置。
+	 */
+	export var config: IConfig;
+
+	/**
+	 * 提供加载全局配置信息的方法。
+	 */
+	export async function loadConfig() {
+
+		// 加载基本配置
+		const configResponse = await fetch('/config.json');
+
+		if (!configResponse.ok) {
+			throw new Error('系统无法读取配置文件，网站无法正常运行');
+		}
+
+		config = await configResponse.json() as IConfig;
+
+		// 加载运行时配置（如果存在）
+		try {
+			const productionConfigResponse = await fetch('/config.production.json');
+			const productionConfig = await productionConfigResponse.json() as IConfig;
+			config = { ...config, ...productionConfig };
+
+		} catch (e) {
+			
+		}
+	}
+
+}
+
+
--- CC98.Forum/CC98.Forum/SignalR.tsx
+++ CC98.Forum/CC98.Forum/SignalR.tsx
@@ -1,5 +1,6 @@
 ﻿import * as SignalR from './SignalRClient/index';
 import { getToken, getApiUrl } from './Utility';
+import * as urljoin from 'url-join';
 
 /**
 * 客户端事件类型，由服务器定义
@@ -13,106 +14,108 @@ type EventListenerType = 'NotifyMessageReceive' | 'NotifyTopicChange' | 'NotifyN
 type EventListenerhandler = (message: any) => void;
 
 class EventListener {
-    NotifyMessageReceive: EventListenerhandler[] = [];
-    NotifyTopicChange: EventListenerhandler[] = [];
-    NotifyNotificationReceive: EventListenerhandler[] = [];
+	NotifyMessageReceive: EventListenerhandler[] = [];
+	NotifyTopicChange: EventListenerhandler[] = [];
+	NotifyNotificationReceive: EventListenerhandler[] = [];
 }
 
 class SignalRConnection {
     /**
      * SignalR服务器地址
      */
-    private readonly _url = `${getApiUrl()}/signalr/notification`;
+	private get url() {
+		return urljoin(getApiUrl(), 'signalr/notification');
+	};
 
     /**
      * 当前connection所用的token
      */
-    private _currentToken: string;
+	private _currentToken: string;
 
     /**
      * 当前正在进行的connection对象
      */
-    private _connection = new SignalR.HubConnection(this._url);
+	private _connection: SignalR.HubConnection;
 
     /**
      * 是否在链接状态
      */
-    private _isConneting: boolean;
+	private _isConneting: boolean;
 
     /**
      * 当前注册在connection上的事件监听
      */
-    private _eventListeners = new EventListener();
+	private _eventListeners = new EventListener();
 
     /**
      * 为SignalR链接注册事件回掉函数
      */
-    public addListener(type: EventListenerType, handler: EventListenerhandler): void {
-        this._eventListeners[type].push(handler);
-        this._connection.on(type, handler);
-    }
+	public addListener(type: EventListenerType, handler: EventListenerhandler): void {
+		this._eventListeners[type].push(handler);
+		this._connection.on(type, handler);
+	}
 
     /**
      * 为SignalR链接删除事件回掉函数
      */
-    public removeListener(type: EventListenerType, handler: EventListenerhandler): void {
-        if (this._eventListeners[type].indexOf(handler) !== -1) {
-            this._eventListeners[type].splice(this._eventListeners[type].indexOf(handler), 1)
-        }
-        this._connection.off(type, handler);
-    }
+	public removeListener(type: EventListenerType, handler: EventListenerhandler): void {
+		if (this._eventListeners[type].indexOf(handler) !== -1) {
+			this._eventListeners[type].splice(this._eventListeners[type].indexOf(handler), 1)
+		}
+		this._connection.off(type, handler);
+	}
 
     /**
      * 向SignalR服务器发送信息
      */
-    public sendMessage(methodName: string, ...message:any[]) {
-        this._connection.invoke(methodName, ...message);
-    }
+	public sendMessage(methodName: string, ...message: any[]) {
+		this._connection.invoke(methodName, ...message);
+	}
 
     /**
      * 开始SignalR链接
      */
-    public async start() {
-        const token = await getToken();
+	public async start() {
+		const token = await getToken();
         /**
         * token更换过就创建一个新的
         * 然后把注册在旧的上的事件监听移到新的上
         */
-        if (this._currentToken !== token) {
-            this._connection = new SignalR.HubConnection(this._url, { jwtBearer: () => token, logging: new SignalR.NullLogger() });
-            let key: EventListenerType;
-            for (key in this._eventListeners) {
-                this._eventListeners[key].map(item => this._connection.on(key, item));
-            }
-        }
+		if (!this._connection || this._currentToken !== token) {
+			this._connection = new SignalR.HubConnection(this.url, { jwtBearer: () => token, logging: new SignalR.NullLogger() });
+			let key: EventListenerType;
+			for (key in this._eventListeners) {
+				this._eventListeners[key].map(item => this._connection.on(key, item));
+			}
+		}
         /**
         * 自动重新开始链接
         */
-        this._connection.onclose((e) => {
-            if (this._isConneting) {
-                this.start();
-            }
-        });
+		this._connection.onclose((e) => {
+			if (this._isConneting) {
+				this.start();
+			}
+		});
 
-        await this._connection.start();
-        this._isConneting = true;
-        
-    }
+		await this._connection.start();
+		this._isConneting = true;
+
+	}
 
     /**
      * 关闭SignalR链接
      */
-    public stop() {
-        this._isConneting = false;
-        this._connection.stop();
-    }
+	public stop() {
+		this._isConneting = false;
+		this._connection.stop();
+	}
 
     /**
      * 
      */
-    public get status() {
-        return this._isConneting;
-    }
+	public get status() {
+		return this._isConneting;
+	}
 }
 
 export default new SignalRConnection();
