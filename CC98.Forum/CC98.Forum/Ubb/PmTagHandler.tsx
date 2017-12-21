3c51782c1ba12f14844715b8bbf9cc5d957be56f
adddna <zzaadddna@live.com>
2017-12-21 19:40:47 +0800

    pm标签

--- CC98.Forum/CC98.Forum/Ubb/PmTagHandler.tsx
+++ CC98.Forum/CC98.Forum/Ubb/PmTagHandler.tsx
@@ -1,6 +1,4 @@
-﻿// /message/message ? name = Dearkano
-
-// A '.tsx' file enables JSX support in the TypeScript compiler, 
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
 
