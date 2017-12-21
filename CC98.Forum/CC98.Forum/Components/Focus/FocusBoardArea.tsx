85444ebaa8a000f50ce95202033d8498ab32849f
明一gg <zhoulm404@gmail.com>
2017-12-21 16:39:59 +0800

    修复

---
+++ CC98.Forum/CC98.Forum/Components/Focus/FocusBoardArea.tsx
@@ -0,0 +1,112 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from 'react';
+import { FocusBoard } from '../../Props/FocusBoard';
+import { FocusBoardAreaState } from '../../States/FocusBoardAreaState';
+import { FocusTopicArea } from './FocusTopicArea';
+import * as Utility from '../../Utility';
+/**
+ * 表示我关注的版面列表区域
+ */
+export class FocusBoardArea extends React.Component<{}, FocusBoardAreaState> {
+
+    /**
+     * 构造函数
+     * @param props
+     */
+    constructor(props) {
+        super(props);
+        //先看一下有没有缓存的帖子数据
+        var data = Utility.getStorage("focusBoardList");
+        if (!data) {
+            data = [];
+        }
+        let currentFocusBoard = Utility.getStorage("currentFocusBoard");
+        let currentBoardId = 0;
+        let currentBoardName = "全站帖子";
+        if (currentFocusBoard) {
+            currentBoardId = currentFocusBoard.boardId;
+            currentBoardName = currentFocusBoard.boardName;
+        }
+        this.state = {
+            data: data,
+            currentBoardId: currentBoardId,
+            currentBoardName: currentBoardName
+        };
+    }
+
+    /**
+     * 将我关注的版面排列好
+     */
+    async componentDidMount() {
+        //给关注版面添加选中效果
+        $('#myFocusUser').removeClass('focus-title-hover');
+        $('#myFocusBoard').addClass('focus-title-hover');
+
+        //先看缓存里有没有关注版面列表的数据
+        let data: FocusBoard[] = Utility.getStorage("focusBoardList");
+        if (data) {
+            this.setState({ data: data });
+        }
+        //没有就自己去服务器获取
+        else {
+            data = [];
+            let token = Utility.getLocalStorage("accessToken");
+            //获取关注版面的id列表
+            let userInfo = Utility.getLocalStorage("userInfo");
+            let boardid = userInfo.customBoards;
+  
+            const headers = new Headers();
+            headers.append('Authorization', token);
+            for (let item of boardid) {
+                let boardName = await Utility.getBoardName(item);
+                data.push({ id: item, name: boardName });
+            }
+            this.setState({ data: data });
+            //存到缓存里
+            Utility.setStorage("focusBoardList", data);
+        }
+
+        //给初始选中版面添加选中效果
+        $(`#focusBoard_${this.state.currentBoardId}`).addClass('focus-hover');
+
+        //给每个版的按钮添加点击事件
+        let self = this;
+        $('.focus-board').click(function () {
+            let boardId = this.id.slice(11);
+            //如果没被点击过，就设为选中，并更新下方帖子列表
+            if (this.className === 'focus-board') {
+                let currentFocusBoard = { boardId: boardId, boardName: this.innerHTML };
+                Utility.setStorage("currentFocusBoard", currentFocusBoard);
+                $('.focus-board').removeClass('focus-hover');
+                $(this).addClass('focus-hover');
+                self.setState({ currentBoardId: parseInt(boardId), currentBoardName: this.innerHTML });
+            }
+            //如果被点击过，就设为取消，并更新下方帖子列表
+            else if (this.className === 'focus-board focus-hover') {
+                Utility.removeStorage('currentFocusBoard');
+                $(this).removeClass('focus-hover');
+                $(`#focusBoard_0`).addClass('focus-hover');
+                self.setState({ currentBoardId: 0, currentBoardName: "全部帖子" });
+            }
+        });
+
+    }
+
+    
+    coverFocusBoard = (item: FocusBoard) => {
+        //点击版面名称会显示相应版面的帖子
+        return <div className="focus-board" id={`focusBoard_${item.id}`}>{item.name}</div>;
+    }
+    
+    render() {
+        return <div>
+                    <div className="focus-board-area">
+                        <div className="focus-board" id="focusBoard_0">全部帖子</div>
+                        {this.state.data.map(this.coverFocusBoard)}
+                    </div>
+                    <FocusTopicArea id={this.state.currentBoardId} name={this.state.currentBoardName} />
+                </div>;
+        }
+}
---
+++ CC98.Forum/CC98.Forum/Components/Focus/FocusTopicSingle.tsx
@@ -0,0 +1,141 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from 'react';
+import { FocusTopic } from '../../Props/FocusTopic';
+import * as Utility from '../../Utility';
+/**
+ * 我关注的某个版面的单个主题
+ */
+export class FocusTopicSingle extends React.Component<FocusTopic> {
+
+    componentDidMount() {
+        if (!this.props.tag1) {
+            $(`#tag_${this.props.id}`).addClass('displaynone');
+        }
+
+        if (!this.props.userId) {
+            $(`#user_${this.props.id}`).removeAttr('href');
+            $(`#user_${this.props.id}`).removeAttr('target');
+        }
+    }
+
+    render() {
+        let tagInfo = '';
+        if (this.props.tag1) {
+            if (this.props.tag2) {
+                tagInfo = `标签：${this.props.tag1} / ${this.props.tag2}`;
+            }
+            else {
+                tagInfo = `标签：${this.props.tag1}`;
+            }
+        }
+        let topicUrl = `/topic/${this.props.id}`;
+        let userUrl = `/user/id/${this.props.userId}`;
+        let boardUrl = `/list/${this.props.boardId}`;
+        let a: any = parseInt(this.props.replyCount+1) / 10;
+        let b = parseInt(a) + 1;
+        let c = parseInt(this.props.replyCount) - (b - 1) * 10;
+        let lastPostUrl = `${topicUrl}/${b}#${c}`;
+        return (<div className="focus-topic">
+                            <a className="focus-topic-left" href={userUrl} target="_blank" id={`user_${this.props.id}`}>
+                                        <img className="focus-topic-portraitUrl" src={this.props.portraitUrl}></img>
+                                        <div className="focus-topic-text">
+                                            <div className="focus-topic-userName">{this.props.userName}</div>
+                                            <div className="focus-topic-fans">{`粉丝   ${this.props.fanCount}`}</div>
+                                        </div>
+                            </a>
+                            <div className="focus-topic-middle">
+                                <a className="focus-topic-title" href={topicUrl} target="_blank">{this.props.title}</a>
+                                <div className="focus-topic-info">
+                                    <div id={`tag_${this.props.id}`}>{tagInfo}</div>
+                                    <div><i className="fa fa-clock-o fa-lg"></i>{this.props.time}</div>
+                                    <div><i className="fa fa-eye fa-lg"></i> {this.props.hitCount}</div>
+                                    <div><a href={lastPostUrl} target="_blank">最后回复：{this.props.lastPostUser}</a></div>
+                                    <div><a href={lastPostUrl} target="_blank">{this.props.lastPostTime}</a></div>
+                                </div>
+                            </div>
+                    <div className="focus-topic-rightBar"></div>
+                    <a className="focus-topic-right" href={boardUrl} target="_blank"><div className="focus-topic-board">{this.props.boardName}</div></a>
+                </div>);
+    }
+}
+
+
+/*
+export class FocusTopicSingle extends React.Component<FocusTopic> {
+   
+    render() {
+        let topicUrl = `/topic/${this.props.id}`;
+        let boardUrl = `/list/${this.props.boardId}`;
+        return (<div className="focus-topic">
+                    <PortaritrUrl userId={this.props.userId} portraitUrl={this.props.portraitUrl} />
+                    <div className="focus-topic-info1">
+                    <div className="focus-topic-authorInfo">
+                    <UserName userId={this.props.userId} userName={this.props.userName} />
+                            <div className="focus-topic-fans">粉丝</div>
+                            <div className="focus-topic-redText">{this.props.fanCount}</div>
+                        </div>
+                        <div className="focus-topic-title"><a href={topicUrl} target="_blank">{this.props.title}</a></div>
+                    </div>
+                    <div className="focus-topic-info2">
+                        <div className="focus-topic-board"><a href={boardUrl} target="_blank">{this.props.boardName}</a>&nbsp;&nbsp;/&nbsp;&nbsp;{moment(this.props.time).format('YYYY-MM-DD HH:mm:ss')}</div>
+                        <div className="focus-topic-response">
+                        <div><i className="fa fa-thumbs-o-up" aria-hidden="true"></i>{this.props.likeCount}</div>
+                    <div><i className="fa fa-thumbs-o-down" aria-hidden="true"></i>{this.props.dislikeCount}</div>
+                            <div><i className="fa fa-eye" aria-hidden="true"></i>{this.props.hitCount}</div>
+                            <div><i className="fa fa-commenting-o" aria-hidden="true"></i>{this.props.replyCount}</div>
+                        </div>
+                    </div>
+                </div>);
+
+    }
+}
+
+
+
+
+//返回可点击或者不可点击的头像
+export class PortaritrUrl extends React.Component<PortaritrUrlProps> {
+    render() {
+        if (this.props.userId) {
+            let userUrl = `/user/id/${this.props.userId}`;
+            return (<a href={userUrl} target="_blank">
+                <img className="focus-topic-portraitUrl" src={this.props.portraitUrl}></img>
+            </a>);
+        }
+        else {
+            return <img className="focus-topic-portraitUrl" src={this.props.portraitUrl}></img>;
+        }
+    }
+}
+
+//返回可点击或者不可点击的用户名
+export class UserName extends React.Component<UserNameProps> {
+    render() {
+        if (this.props.userId) {
+            let userUrl = `/user/id/${this.props.userId}`;
+            return (<a href={userUrl} target="_blank" className="focus-topic-blackText">
+                        <div className="focus-topic-blackText">{this.props.userName}</div>
+                    </a>);
+        }
+        else {
+            return <div className="focus-topic-blackText">{this.props.userName}</div>;
+        }
+    }
+}
+
+
+export class PortaritrUrlProps {
+    //用户id
+    userId: number;
+    //用户头像地址
+    portraitUrl: string;
+}
+
+export class UserNameProps {
+    //用户id
+    userId: number;
+    //用户名称
+    userName: string;
+}*/
