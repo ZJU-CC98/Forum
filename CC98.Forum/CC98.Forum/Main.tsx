c67525a206f56ab69f44e7551322f8b682e976ed
AsukaSong <yansongdong@gmail.com>
2018-01-04 18:27:51 +0800

    修

--- CC98.Forum/CC98.Forum/Main.tsx
+++ CC98.Forum/CC98.Forum/Main.tsx
@@ -1,8 +1,28 @@
 ﻿import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as $ from 'jquery';
+import store from './Store';
+import { Provider } from 'react-redux';
+import 'whatwg-fetch';
 
+import { Constants } from './Components/Constant';
 import { App } from './Components/App';
 
-// 显示应用程序核心内容
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * 项目初始化代码
+ */
+async function initialize() {
+
+	await Constants.loadConfig();
+
+	// 显示应用程序核心内容
+	ReactDOM.render(
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		document.getElementById('root')
+
+	);
+}
+
+initialize();
--- CC98.Forum/CC98.Forum/Match/Match.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MessageMatch = (function () {
-    function MessageMatch() {
-    }
-    return MessageMatch;
-}());
-exports.MessageMatch = MessageMatch;
-//# sourceMappingURL=Match.js.map
