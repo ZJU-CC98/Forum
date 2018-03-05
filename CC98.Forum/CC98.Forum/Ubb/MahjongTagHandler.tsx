794d5a6639171196f1ed22337afbc5db5775d0d2
田子珺 <tzj912016486@vip.qq.com>
2018-03-05 13:49:15 +0800

    js到本地

--- CC98.Forum/CC98.Forum/Ubb/MahjongTagHandler.tsx
+++ CC98.Forum/CC98.Forum/Ubb/MahjongTagHandler.tsx
@@ -1,4 +1,4 @@
-﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+﻿﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
 
