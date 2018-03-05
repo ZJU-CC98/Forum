b3e7a06de37438684094cb9fe615ec26f49dadc5
AsukaSong <yansongdong@gmail.com>
2018-03-05 21:26:37 +0800

    进一步整合js文件
    精简index.html
    兼容性检查移至main.tsx

--- CC98.Forum/CC98.Forum/webpack.config.debug.ts
+++ CC98.Forum/CC98.Forum/webpack.config.debug.ts
@@ -46,17 +46,12 @@ const config: webpack.Configuration = {
 			{ from: 'node_modules/jquery/dist', to: 'static/scripts/lib/jquery/' },
 			{ from: 'node_modules/moment', to: 'static/scripts/lib/moment/' },
 			{ from: 'node_modules/bootstrap/dist', to: 'static/scripts/lib/bootstrap/' },
-			{ from: 'node_modules/es6-promise/dist', to: 'static/scripts/lib/es6-promise/' },
 			{ from: 'node_modules/font-awesome', to: 'static/content/font-awesome/' },
 			{ from: 'node_modules/moment', to: 'static/scripts/lib/moment/' },
 			{ from: 'node_modules/editor.md', to: 'static/scripts/lib/editor.md/' },
 			{ from: 'node_modules/codemirror', to: 'static/scripts/lib/editor.md/lib/codemirror/' },
-			{ from: 'node_modules/blueimp-canvas-to-blob/js', to: 'static/scripts/lib/blueimp-canvas-to-blob/' },
-			{ from: 'node_modules/@aspnet/signalr-client/dist/browser', to: 'static/scripts/lib/signalr-client/' },
 			{ from: 'node_modules/spectrum-colorpicker/spectrum.js', to: 'static/scripts/lib/spectrum/spectrum.js' },
-            { from: 'node_modules/url-join/', to: 'static/scripts/lib/url-join/' },
-			{ from: 'node_modules/aplayer/dist/', to: 'static/scripts/lib/aplayer/' },
-			{ from: 'node_modules/dplayer/dist/', to: 'static/scripts/lib/dplayer/' }
+			{ from: 'node_modules/dplayer/dist/DPlayer.min.css', to: 'static/content/DPlayer.min.css' }
 		]),
 		new ExtractTextPlugin('static/content/site.min.css')
 	]
