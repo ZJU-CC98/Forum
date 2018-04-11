dae2020f8976407fb120f32bd644ca5bdabd6fa2
AsukaSong <yansongdong@gmail.com>
2018-04-11 18:10:19 +0800

    添加IndexedDB的接口

--- CC98.Forum/CC98.Forum/Main.tsx
+++ CC98.Forum/CC98.Forum/Main.tsx
@@ -10,7 +10,7 @@ import 'blueimp-canvas-to-blob';
 
 import { Constants } from './Components/Constant';
 import { App } from './Components/App';
-import { autoClearLocalStorage } from './Utility';
+// import { IndexedDB } from './IndexedDB/IndexedDB';
 
 /**
  * 项目初始化代码
@@ -18,6 +18,7 @@ import { autoClearLocalStorage } from './Utility';
 async function initialize() {
 
 	await Constants.loadConfig();
+	// await IndexedDB.start();
 
 	// 输出一些没用的东西
 	console.info('%c       ', 'font-size: 100px; background: url(http://cdn.nyanit.com/nyan2.gif) no-repeat;');
