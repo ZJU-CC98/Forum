8b06a8446db4b1f9cd362c03318fcc7e67bdfc6a
AsukaSong <yansongdong@gmail.com>
2018-06-13 18:04:59 +0800

    添加吉祥物的头像框

--- CC98.Forum/CC98.Forum/Components/UserCenter/ExactAvatar.tsx
+++ CC98.Forum/CC98.Forum/Components/UserCenter/ExactAvatar.tsx
@@ -47,40 +47,39 @@ export default class extends React.Component<UserCenterExactAvatarProps, State> 
         let title = this.state.displayTitles.filter(item => this.props.userInfo.displayTitleId === item.id)[0]
         if(title) {
             let imageUrl: string;
-            switch (this.props.userInfo.displayTitleId) {
-                case 82: imageUrl = data.吉祥物.imageUrl; break;
-                case 18: imageUrl = data.版主.imageUrl; break;
-                case 22: imageUrl = data.版主.imageUrl; break;
-                case 85: imageUrl = data.编辑部.imageUrl; break;
-                case 29: imageUrl = data.编辑部.imageUrl; break;
-                case 37: imageUrl = data.技术组.imageUrl; break;
-                case 23: imageUrl = data.技术组.imageUrl; break;
-                case 28: imageUrl = data.贵宾.imageUrl; break;
-                case 16: imageUrl = data.贵宾.imageUrl; break;
-                case 84: imageUrl = data.策划部.imageUrl; break;
-                case 34: imageUrl = data.策划部.imageUrl; break;
-                case 96: imageUrl = data.影音部.imageUrl; break;
-                case 99: imageUrl = data.影音部.imageUrl; break;
-                case 32: imageUrl = data.站务组.imageUrl; break;
-                case 21: imageUrl = data.站务组.imageUrl; break;
-                case 86: imageUrl = data.体育部.imageUrl; break;
-                case 35: imageUrl = data.体育部.imageUrl; break;
-                case 94: imageUrl = data.办公室.imageUrl; break;
-                case 93: imageUrl = data.办公室.imageUrl; break;
-                case 91: imageUrl = data.认证用户.imageUrl; break;
-                default: imageUrl = data.普通.imageUrl;
+            if(this.props.userInfo.displayTitleId === 82) {
+                imageUrl = data.吉祥物.imageUrl;
+                // case 18: imageUrl = data.版主.imageUrl; break;
+                // case 22: imageUrl = data.版主.imageUrl; break;
+                // case 85: imageUrl = data.编辑部.imageUrl; break;
+                // case 29: imageUrl = data.编辑部.imageUrl; break;
+                // case 37: imageUrl = data.技术组.imageUrl; break;
+                // case 23: imageUrl = data.技术组.imageUrl; break;
+                // case 28: imageUrl = data.贵宾.imageUrl; break;
+                // case 16: imageUrl = data.贵宾.imageUrl; break;
+                // case 84: imageUrl = data.策划部.imageUrl; break;
+                // case 34: imageUrl = data.策划部.imageUrl; break;
+                // case 96: imageUrl = data.影音部.imageUrl; break;
+                // case 99: imageUrl = data.影音部.imageUrl; break;
+                // case 32: imageUrl = data.站务组.imageUrl; break;
+                // case 21: imageUrl = data.站务组.imageUrl; break;
+                // case 86: imageUrl = data.体育部.imageUrl; break;
+                // case 35: imageUrl = data.体育部.imageUrl; break;
+                // case 94: imageUrl = data.办公室.imageUrl; break;
+                // case 93: imageUrl = data.办公室.imageUrl; break;
+                // case 91: imageUrl = data.认证用户.imageUrl; break;
+                this.setState({
+                    url: imageUrl
+                })
             }
-            this.setState({
-                url: imageUrl
-            })
         }
     }
 
     render() {
         return (
-            <div className="user-avatar">
+            <div style={{ position: 'relative' }} className="user-avatar">
                 <img className="user-avatar-img" src={this.props.userInfo.portraitUrl} />
-                {/* <img src={this.state.url} /> */}
+                {this.state.url ? <img style={{ width: '13rem', height: '13rem', position: 'absolute', top: '-1.5rem' }} src={this.state.url} /> : null}
                 <div className="user-badge">
                     {this.state.displayTitles && this.props.userInfo.userTitleIds ? 
                         this.state.displayTitles.filter(item => this.props.userInfo.userTitleIds.indexOf(item.id) !== -1).map(item => [18, 81].indexOf(item.id) === -1 ? 
