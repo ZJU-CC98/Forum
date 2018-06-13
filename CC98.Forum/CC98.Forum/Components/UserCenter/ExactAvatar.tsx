a9886c008429a46e91942914fa9ca30686d68ff5
AsukaSong <yansongdong@gmail.com>
2018-06-13 17:51:56 +0800

    个人中心添加userTitle

--- CC98.Forum/CC98.Forum/Components/UserCenter/ExactAvatar.tsx
+++ CC98.Forum/CC98.Forum/Components/UserCenter/ExactAvatar.tsx
@@ -3,16 +3,91 @@
 // https://github.com/Microsoft/TypeScript/wiki/JSX
 
 import * as React from 'react';
+import * as Utility from '../../Utility';
 import  { UserInfo } from '../../States/AppState';
 import { Link } from 'react-router-dom';
 
+interface State {
+    displayTitles: DispalyTitle[];
+    url: string;
+}
+
+interface DispalyTitle {
+    id: number;
+    name: string;
+}
+
 //用户中心主页用户头像与徽章组件
-export default class extends React.Component<UserCenterExactAvatarProps> {
+export default class extends React.Component<UserCenterExactAvatarProps, State> {
+    state: State = {
+        displayTitles: [],
+        url: ''
+    }
+
+    componentDidMount() {
+        this.getTitle();
+    }
+
+    async getTitle() {
+        try {
+            const url = '/config/global/all-user-title';
+            let res = await Utility.cc98Fetch(url);
+            let data = await res.json() as DispalyTitle[];
+            this.setState({
+                displayTitles: data
+            }, this.getPortraitPhotoURL);
+        } catch (e) {
+
+        }
+    }
+
+    async getPortraitPhotoURL() {
+        let res = await fetch('/static/portrait.json');
+        let data = await res.json()
+        let title = this.state.displayTitles.filter(item => this.props.userInfo.displayTitleId === item.id)[0]
+        if(title) {
+            let imageUrl: string;
+            switch (this.props.userInfo.displayTitleId) {
+                case 82: imageUrl = data.吉祥物.imageUrl; break;
+                case 18: imageUrl = data.版主.imageUrl; break;
+                case 22: imageUrl = data.版主.imageUrl; break;
+                case 85: imageUrl = data.编辑部.imageUrl; break;
+                case 29: imageUrl = data.编辑部.imageUrl; break;
+                case 37: imageUrl = data.技术组.imageUrl; break;
+                case 23: imageUrl = data.技术组.imageUrl; break;
+                case 28: imageUrl = data.贵宾.imageUrl; break;
+                case 16: imageUrl = data.贵宾.imageUrl; break;
+                case 84: imageUrl = data.策划部.imageUrl; break;
+                case 34: imageUrl = data.策划部.imageUrl; break;
+                case 96: imageUrl = data.影音部.imageUrl; break;
+                case 99: imageUrl = data.影音部.imageUrl; break;
+                case 32: imageUrl = data.站务组.imageUrl; break;
+                case 21: imageUrl = data.站务组.imageUrl; break;
+                case 86: imageUrl = data.体育部.imageUrl; break;
+                case 35: imageUrl = data.体育部.imageUrl; break;
+                case 94: imageUrl = data.办公室.imageUrl; break;
+                case 93: imageUrl = data.办公室.imageUrl; break;
+                case 91: imageUrl = data.认证用户.imageUrl; break;
+                default: imageUrl = data.普通.imageUrl;
+            }
+            this.setState({
+                url: imageUrl
+            })
+        }
+    }
+
     render() {
         return (
             <div className="user-avatar">
                 <img className="user-avatar-img" src={this.props.userInfo.portraitUrl} />
+                {/* <img src={this.state.url} /> */}
                 <div className="user-badge">
+                    {this.state.displayTitles && this.props.userInfo.userTitleIds ? 
+                        this.state.displayTitles.filter(item => this.props.userInfo.userTitleIds.indexOf(item.id) !== -1).map(item => [18, 81].indexOf(item.id) === -1 ? 
+                            <p style={{ color: 'red' }}>{item.name}</p> : 
+                            null) :
+                        null
+                    }
                     {this.props.userInfo.boardMasterTitles.map(item => item.boardMasterLevel === 10 ?
                         <p key={item.boardId}> {/* 站务主管*/}
                             <Link to={`/boardList#${item.boardName}`}>{item.boardName}</Link>
