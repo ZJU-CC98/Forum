77e2b7a2c5f4759e8ee1d0f871f825e1fd7d7a5d
Iris Sakura <sgjsakura@hotmail.com>
2018-08-17 21:23:24 +0800

    修复 webpack 错误

--- CC98.Forum/CC98.Forum/webpack.config.release.ts
+++ CC98.Forum/CC98.Forum/webpack.config.release.ts
@@ -78,7 +78,6 @@ const config: Webpack.Configuration = {
 			{ from: 'node_modules/jquery/dist', to: 'static/scripts/lib/jquery/' },
 			{ from: 'node_modules/moment', to: 'static/scripts/lib/moment/' },
 			{ from: 'node_modules/font-awesome', to: 'static/content/font-awesome/' },
-			{ from: 'node_modules/moment', to: 'static/scripts/lib/moment/' },
 			{ from: 'node_modules/editor.md', to: 'static/scripts/lib/editor.md/' },
 			{ from: 'node_modules/codemirror', to: 'static/scripts/lib/editor.md/lib/codemirror/' },
 			{ from: 'node_modules/spectrum-colorpicker/spectrum.js', to: 'static/scripts/lib/spectrum/spectrum.js' },
