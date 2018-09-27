ad2ceef41f20b61361d4d9e10e3e9fba2a088deb
Hydrogen <fanyangliang@gmail.com>
2018-09-28 02:14:16 +0800

    pref: remove some useless reRender

--- CC98.Forum/CC98.Forum/Components/NoticeMessage.tsx
+++ CC98.Forum/CC98.Forum/Components/NoticeMessage.tsx
@@ -8,7 +8,7 @@ import * as React from 'react';
  * top: 消息提示距顶部的距离
  * left:消息提示距左边的距离
  */
-export class NoticeMessage extends React.Component<{ text: string; id: string; top: string; left: string }> {
+export class NoticeMessage extends React.PureComponent<{ text: string; id: string; top: string; left: string }> {
     render() {
         return <div className="noticeSuccess displaynone" id={this.props.id} style={{ position: 'fixed', top: this.props.top, left: this.props.left }}>{this.props.text}</div>;
     }
