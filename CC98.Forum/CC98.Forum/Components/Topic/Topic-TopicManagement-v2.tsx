ce54297f72dee7470350b92e6a8b1514b3448aa5
zzaadddna <zzaadddna@live.com>
2018-10-07 21:31:13 +0800

    fix：删除帖子管理中被错误添加的会引起chrome以外浏览器报错的偶像厨走马灯

--- CC98.Forum/CC98.Forum/Components/Topic/Topic-TopicManagement-v2.tsx
+++ CC98.Forum/CC98.Forum/Components/Topic/Topic-TopicManagement-v2.tsx
@@ -350,13 +350,7 @@ export default Form.create<Props>()(class extends React.Component<Props & FormCo
                     <Item key="7">{this.props.topicInfo.topState === 4 ? '取消全站固顶' : '全站固顶'}</Item>
                     <Item key="8">{this.props.topicInfo.bestState === 1 ? "解除精华" : "加精"}</Item>
                     <Item key="9">高亮</Item>
-                </Menu>
-                <Carousel effect="fade">
-                <div><img src="http://file.cc98.org/v2-upload/se0ekkk2.jpg"></img></div>
-                <div><img src="http://file.cc98.org/v2-upload/u00151gl.jpg"></img></div>
-                <div><img src="http://file.cc98.org/v2-upload/wbvcn5ml.jpg"></img></div>
-                <div><img src="http://file.cc98.org/v2-upload/ir23l2ai.jpg"></img></div>
-            </Carousel>
+                </Menu>               
                 {rcForm}
             </Modal>
         )
