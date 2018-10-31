74fcffe90cb1d0ce4771c32103f0c67f2739b187
Dearkano <vaynetian@cc98.org>
2018-10-31 20:00:19 +0800

    fix 改为remark解析

--- CC98.Forum/CC98.Forum/Components/Topic/Topic-ReplyContent.tsx
+++ CC98.Forum/CC98.Forum/Components/Topic/Topic-ReplyContent.tsx
@@ -8,7 +8,7 @@ import { UbbCodeOptions } from '../../Ubb/UbbCodeExtension';
 import { VoteContent, voteInfo } from './VoteInfo';
 var remark = require('remark');
 var reactRenderer = require('remark-react');
-declare let editormd: any;
+
 interface Props {
     postId;
     content;
@@ -29,30 +29,30 @@ export class ReplyContent extends React.Component<Props, { postId, vote: voteInf
     }
     async componentDidMount() {
         if (this.props.floor === 1 && this.props.topicInfo.isVote && !this.state.vote) this.getVote();
-        const divid = `doc-content${this.props.postId}`;
-        editormd.markdownToHTML(divid, {
-            htmlDecode: false,
-            emoji: true,
-            taskList: true,
-            tex: true,
-            flowChart: true,
-            sequenceDiagram: true,
-            codeFold: true,
-        });
+        // const divid = `doc-content${this.props.postId}`;
+        // editormd.markdownToHTML(divid, {
+        //     htmlDecode: false,
+        //     emoji: true,
+        //     taskList: true,
+        //     tex: true,
+        //     flowChart: true,
+        //     sequenceDiagram: true,
+        //     codeFold: true,
+        // });
     }
 
-    componentDidUpdate() {
-        const divid = `doc-content${this.props.postId}`;
-        editormd.markdownToHTML(divid, {
-            htmlDecode: false,
-            emoji: true,
-            taskList: true,
-            tex: true,
-            flowChart: true,
-            sequenceDiagram: true,
-            codeFold: true,
-        });
-    }
+    // componentDidUpdate() {
+    //     const divid = `doc-content${this.props.postId}`;
+    //     editormd.markdownToHTML(divid, {
+    //         htmlDecode: false,
+    //         emoji: true,
+    //         taskList: true,
+    //         tex: true,
+    //         flowChart: true,
+    //         sequenceDiagram: true,
+    //         codeFold: true,
+    //     });
+    // }
     async getVote() {
         try {
             let headers = await Utility.formAuthorizeHeader();
@@ -75,24 +75,24 @@ export class ReplyContent extends React.Component<Props, { postId, vote: voteInf
         const ubbMode = <UbbContainer code={ubbUrlContent} options={{ ...new UbbCodeOptions(), allowLightbox: true }} />;
         let mdUrlContent = Utility.atUserMdUrl(this.props.content);
 
-        // // 鬼畜正则代码  兼容老版本md语法解析问题
-        // let parseContent = this.props.content.replace(/\n>[\s\S]*?\n\n/g, (v) => v.replace(/\n[^\n](?!>)/g, (v1) => v1.replace(/\n(?!>)/, '\n>')));
-        // if (parseContent[0] === '>') {
-        //     const index = parseContent.indexOf('\n\n');
-        //     if (index === -1) {
-        //         parseContent = parseContent.replace(/\n[^\n](?!>)/g, (v1) => v1.replace(/\n(?!>)/, '\n>'));
-        //     } else {
-        //         const substr = parseContent.substr(0, index);
-        //         parseContent = substr.replace(/\n[^\n](?!>)/g, (v1) => v1.replace(/\n(?!>)/, '\n>')) + parseContent.substr(index + 1, parseContent.length);
-        //     }
-        // }
-        // parseContent = parseContent.replace(/发言：\*\*\n/g, "发言：**\n\n");
-        // const mdMode = <div id={domId} style={{maxWidth:"52rem",overflow:"hidden"}}>
-        //     {remark().use(reactRenderer).processSync(parseContent).contents}
-        // </div>;
-        const mdMode = <div id={domId}>
-        <textarea name="editormd-markdown-doc" style={{ display: 'none' }}>{mdUrlContent}</textarea>
+        // 鬼畜正则代码  兼容老版本md语法解析问题
+        let parseContent = this.props.content.replace(/\n>[\s\S]*?\n\n/g, (v) => v.replace(/\n[^\n](?!>)/g, (v1) => v1.replace(/\n(?!>)/, '\n>')));
+        if (parseContent[0] === '>') {
+            const index = parseContent.indexOf('\n\n');
+            if (index === -1) {
+                parseContent = parseContent.replace(/\n[^\n](?!>)/g, (v1) => v1.replace(/\n(?!>)/, '\n>'));
+            } else {
+                const substr = parseContent.substr(0, index);
+                parseContent = substr.replace(/\n[^\n](?!>)/g, (v1) => v1.replace(/\n(?!>)/, '\n>')) + parseContent.substr(index + 1, parseContent.length);
+            }
+        }
+        parseContent = parseContent.replace(/发言：\*\*\n/g, "发言：**\n\n");
+        const mdMode = <div id={domId} style={{maxWidth:"52rem",overflow:"hidden"}}>
+            {remark().use(reactRenderer).processSync(parseContent).contents}
         </div>;
+        // const mdMode = <div id={domId}>
+        // <textarea name="editormd-markdown-doc" style={{ display: 'none' }}>{mdUrlContent}</textarea>
+        // </div>;
         let content;
         //ubb    
         content = ubbMode;
