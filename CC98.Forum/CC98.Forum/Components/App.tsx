710428d7960977f8bb0485431a8a999a9adc1410
vaynetian <vaynetian@cc98.org>
2018-12-20 00:46:21 +0800

    fix: 取消signalr

--- CC98.Forum/CC98.Forum/Components/App.tsx
+++ CC98.Forum/CC98.Forum/Components/App.tsx
@@ -1,11 +1,7 @@
 ﻿import * as React from 'react';
 import { AppState } from '../States/AppState';
 import { match } from 'react-router';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { history } from '../Store';
 import { Post, ShowTopic } from './Topic/Topic';
@@ -41,56 +37,76 @@ import { errorKeys } from '../Reducers/Error';
 import { connect } from 'react-redux';
 
 type Props = {
-    isError: boolean,
-    errorMessage: errorKeys
+  isError: boolean;
+  errorMessage: errorKeys;
 };
 
 class App extends React.Component<Props> {
-    render() {
-        let router = (
-            <Switch>
-                <Route exact path="/" component={MainPage}></Route>
-                <Route exact path="/topic/hot-monthly" component={MonthlyHotTopic} />
-                <Route exact path="/topic/hot-weekly" component={WeeklyHotTopic} />
-                <Route exact path="/topic/hot-history" component={HistoryHotTopic} />
-                <Route exact path="/topic/:topicid/:page?" component={ShowTopic} />
-                <Route exact path="/topic/:topicId/postid/:postId/:page?" component={CurUserPost} />
-                <Route path="/list/:boardId/:type?/:page?" component={BList} />
-                <Route exact path="/boardlist" component={BoardList} />
-                <Route path="/usercenter" component={UserCenter} />
-                <Route path="/message" component={Message} />
-                <Route path="/focus" component={Focus} />
-                <Route path="/newtopics" component={AllNewTopic} />
-                <Route path="/user/:method/:id" component={User} />
-                <Route path="/logon" exact component={LogOn} />
-                <Route path="/search" component={Search} />
-                <Route path="/searchBoard" component={SearchBoard} />
-                <Route path="/signin" component={Signin} />
-                <Route path="/sitemanage" component={SiteManage} />
-                <Route path="/editor/:mode/:id?" component={ShowEdit} />
-                <Route component={Status.PageNotFound} />
-            </Switch>);
+  render() {
+    let router = (
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/topic/hot-monthly" component={MonthlyHotTopic} />
+        <Route exact path="/topic/hot-weekly" component={WeeklyHotTopic} />
+        <Route exact path="/topic/hot-history" component={HistoryHotTopic} />
+        <Route exact path="/topic/:topicid/:page?" component={ShowTopic} />
+        <Route
+          exact
+          path="/topic/:topicId/postid/:postId/:page?"
+          component={CurUserPost}
+        />
+        <Route path="/list/:boardId/:type?/:page?" component={BList} />
+        <Route exact path="/boardlist" component={BoardList} />
+        <Route path="/usercenter" component={UserCenter} />
+        <Route path="/message" component={Message} />
+        <Route path="/focus" component={Focus} />
+        <Route path="/newtopics" component={AllNewTopic} />
+        <Route path="/user/:method/:id" component={User} />
+        <Route path="/logon" exact component={LogOn} />
+        <Route path="/search" component={Search} />
+        <Route path="/searchBoard" component={SearchBoard} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/sitemanage" component={SiteManage} />
+        <Route path="/editor/:mode/:id?" component={ShowEdit} />
+        <Route component={Status.PageNotFound} />
+      </Switch>
+    );
 
-
-        return (<div style={{ width: "100%" }}>
-            <ConnectedRouter history={history}>
-                <div style={{ justifyContent: 'center', display: 'flex', flexDirection: 'column', alignItems: "center", width: "100%", minWidth: "1140px", backgroundColor: "#e6e7ec" }}>
-                    <Header />
-                    <Route component={ErrorControl} />
-                    {!this.props.isError ? router : <Route component={Status[this.props.errorMessage]} />}
-                    <Footer />
-                    <NotificationController />
-                </div>
-            </ConnectedRouter>
-        </div>);
-    }
+    return (
+      <div style={{ width: '100%' }}>
+        <ConnectedRouter history={history}>
+          <div
+            style={{
+              justifyContent: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%',
+              minWidth: '1140px',
+              backgroundColor: '#e6e7ec'
+            }}
+          >
+            <Header />
+            <Route component={ErrorControl} />
+            {!this.props.isError ? (
+              router
+            ) : (
+              <Route component={Status[this.props.errorMessage]} />
+            )}
+            <Footer />
+            {/* <NotificationController />*/}
+          </div>
+        </ConnectedRouter>
+      </div>
+    );
+  }
 }
 
 function mapState(state: RootState) {
-    return {
-        isError: state.error.isError,
-        errorMessage: state.error.errorMessage
-    };
+  return {
+    isError: state.error.isError,
+    errorMessage: state.error.errorMessage
+  };
 }
 
 export default connect(mapState)(App);
