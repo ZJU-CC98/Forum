37b32e570c02204ae78faa7786ee8eaf78c6cf04
Dearkano <vaynetian@cc98.org>
2019-01-15 23:42:19 +0800

    fix:  个人中心css

--- CC98.Forum/CC98.Forum/Components/UserCenter/Navigation.tsx
+++ CC98.Forum/CC98.Forum/Components/UserCenter/Navigation.tsx
@@ -54,23 +54,23 @@ export default class extends React.Component<props, UserCenterNavigationState> {
     render() {
         return (<div className='user-center-navigation' id="userCenterNavigation" >
             <ul>
-                <li><NavLink to="/usercenter" exact activeClassName="user-center-navigation-active" className="fa-home"><p>个人主页</p></NavLink></li>
+                <li><NavLink to="/usercenter" exact activeClassName="user-center-navigation-active" className="fa-home"><div className="center-nav-item">个人主页</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/config" activeClassName="user-center-navigation-active" className="fa-cog"><p>修改资料</p></NavLink></li>
+                <li><NavLink to="/usercenter/config" activeClassName="user-center-navigation-active" className="fa-cog"><div className="center-nav-item">修改资料</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/myposts" activeClassName="user-center-navigation-active" className="fa-pencil-square-o"><p>我的主题</p></NavLink></li>
+                <li><NavLink to="/usercenter/myposts" activeClassName="user-center-navigation-active" className="fa-pencil-square-o"><div className="center-nav-item">我的主题</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/myfavorites" activeClassName="user-center-navigation-active" className="fa-star"><p>我的收藏</p></NavLink></li>
+                <li><NavLink to="/usercenter/myfavorites" activeClassName="user-center-navigation-active" className="fa-star"><div className="center-nav-item">我的收藏</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/mycustomboards" activeClassName="user-center-navigation-active" className="fa-rss"><p>关注版面</p></NavLink></li>
+                <li><NavLink to="/usercenter/mycustomboards" activeClassName="user-center-navigation-active" className="fa-rss"><div className="center-nav-item">关注版面</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/myfollowings" activeClassName="user-center-navigation-active" className="fa-heart"><p>关注用户</p></NavLink></li>
+                <li><NavLink to="/usercenter/myfollowings" activeClassName="user-center-navigation-active" className="fa-heart"><div className="center-nav-item">关注用户</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/myfans" activeClassName="user-center-navigation-active" className="fa-users"><p>我的粉丝</p></NavLink></li>
+                <li><NavLink to="/usercenter/myfans" activeClassName="user-center-navigation-active" className="fa-users"><div className="center-nav-item">我的粉丝</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/transferwealth" activeClassName="user-center-navigation-active" className="fa-credit-card"><p>转账系统</p></NavLink></li>
+                <li><NavLink to="/usercenter/transferwealth" activeClassName="user-center-navigation-active" className="fa-credit-card"><div className="center-nav-item">转账系统</div></NavLink></li>
                 <hr />
-                <li><NavLink to="/usercenter/theme" activeClassName="user-center-navigation-active" className="fa-magic"><p>切换皮肤</p></NavLink></li> 
+                <li><NavLink to="/usercenter/theme" activeClassName="user-center-navigation-active" className="fa-magic"><div className="center-nav-item">切换皮肤</div></NavLink></li> 
             </ul>
             <button type="button" id="scrollToTop" className={this.state.buttonClassName} onClick={this.scrollToTop}>回到顶部</button>
         </div>);
