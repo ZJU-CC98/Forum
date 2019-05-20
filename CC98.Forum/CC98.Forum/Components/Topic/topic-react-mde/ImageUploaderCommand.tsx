5c3652da726054819e5b3b1c6dade79f8bf765f6
Dearkano <vaynetian@cc98.org>
2019-05-20 18:49:26 +0800

    fix: md上传图片

--- CC98.Forum/CC98.Forum/Components/Topic/topic-react-mde/ImageUploaderCommand.tsx
+++ CC98.Forum/CC98.Forum/Components/Topic/topic-react-mde/ImageUploaderCommand.tsx
@@ -4,16 +4,17 @@ import * as React from 'react';
 let url = "";
 async function handleUpload(e, o) {
     url = await Utility.uploadEvent(e);
-    const instance = o.default.editor
+    const instance = o.editor
     const text = `\n\n![](${url})\n\n`
     instance.setValue(text)
 }
 
 const customCommand = {
-    buttonContentBuilder: ({ iconProvider }) =>
+    buttonContentBuilder: (t) =>
         <div>
-            <label id='upload-label' style={{ cursor: "pointer",paddingBottom:"0.5rem",paddingTop:"0,5rem",paddingLeft:"1rem",paddingRight:"1rem" }} htmlFor="upload-files"><i className={`fa fa-image`} /></label>
-            <input type="file" id="upload-files" style={{ display: 'none ' }} onChange={async e => { const input=e.target;   await handleUpload(e, this);input.value='';} } />
+            <label id='upload-label' style={{ cursor: "pointer", paddingBottom: "0.5rem", paddingTop: "0,5rem", paddingLeft: "1rem", paddingRight: "1rem" }} htmlFor="upload-files"><i className={`fa fa-image`} /></label>
+            <input type="file" id="upload-files" style={{ display: 'none ' }} 
+            onChange={async e => { const input = e.target;  await handleUpload(e, customCommand); input.value = ''; }} />
         </div>,
     buttonProps: {
         handleSubmit:
