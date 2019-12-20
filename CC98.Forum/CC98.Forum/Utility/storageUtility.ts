caed050dfeae787c4df7ea87ce9172b98c6cb0af
Dearkano <vaynetian@cc98.org>
2019-12-20 17:56:05 +0800

    fix: localstorage

--- CC98.Forum/CC98.Forum/Utility/storageUtility.ts
+++ CC98.Forum/CC98.Forum/Utility/storageUtility.ts
@@ -78,6 +78,8 @@ export function getLocalStorage(key) {
     if (v.indexOf('obj-') === 0) {
         v = v.slice(4);
         const obj = JSON.parse(v);
+        console.log('===')
+        console.log(typeof obj)
         // 锁定用户检测退出登录
         if (key === 'userInfo') {
             if (obj.lockState !== 0) {
