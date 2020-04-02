1ae75fc102f1645a272366ca5c2cdad7c0cd7c3a
adddna <zzaadddna@live.com>
2020-04-03 03:12:54 +0800

    fix: bili标签对b站url的解析

--- CC98.Forum/CC98.Forum/Ubb/BiliTagHandler.tsx
+++ CC98.Forum/CC98.Forum/Ubb/BiliTagHandler.tsx
@@ -6,6 +6,22 @@ export class BiliTagHandler extends Ubb.TextTagHandler {
 		return 'bili';
 	}
 
+	/** 根据B站url，获取分p数
+      * @param  {[string]} url B站url 
+      * @return {[string]} 分p数，默认为1
+      */
+	private getBiliPage(url: string): string {
+		let paramstr = url.split("?")[1];
+		let params = paramstr ? paramstr.split("&") : "";
+		for (let i = 0; i < params.length; i++) {
+			let pair = params[i].split("=");
+			if (pair[0] == "p") {
+				return pair[1];
+			}
+		}
+		return "1";
+	}
+
 	execCore(innerContent: string, tagData: Ubb.UbbTagData, context: Ubb.UbbCodeContext): React.ReactNode {
 		//不允许显示媒体内容
 		if (context.options.allowMediaContent === false) {
@@ -13,20 +29,44 @@ export class BiliTagHandler extends Ubb.TextTagHandler {
 		}
 
 		// 分P数，默认为1
-		const partNumber = parseInt(tagData.value('bili')) || 1;
+		let partNumber = parseInt(tagData.value('bili')) || 1;
 
 		let src = "";
 		const isPureNumber = /^\d+$/.test(innerContent);
-		const isBV = /^BV/.test(innerContent);
+		const isBVString = /^BV[A-Za-z0-9]+/.test(innerContent);
+		const isUrl = /(https?:\/\/)?www.bilibili.com\/video\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/.test(innerContent);
+
 		//纯数字依然解析成aid
 		if (isPureNumber) {
 			src = `https://player.bilibili.com/player.html?aid=${innerContent}&page=${partNumber}`;
 		}
 		//BV开头的字符串，解析成bvid
-		else if (isBV) {
+		else if (isBVString) {
 			src = `https://player.bilibili.com/player.html?bvid=${innerContent}&page=${partNumber}`;
 		}
-		//输入错误的字符串，不解析
+		//bili标签内直接复制b站url，根据具体情况做不同的解析
+		else if (isUrl) {
+			//判断是否是av号
+			const isAv = /^(https?:\/\/)?www\.bilibili\.com\/video\/av[0-9]+/.test(innerContent);
+			//判断是否是BV号
+			const isBV = /^(https?:\/\/)?www\.bilibili\.com\/video\/BV[0-9A-Za-z]+/.test(innerContent);
+			//获取url中分P数
+			const page = this.getBiliPage(innerContent);
+
+			if (isAv) {
+				let av = innerContent.split("bilibili.com/video/av")[1].split("?")[0].split("/")[0];
+				src = `https://player.bilibili.com/player.html?aid=${av}&page=${page}`;
+			}
+			else if (isBV) {
+				let BV = innerContent.split("bilibili.com/video/")[1].split("?")[0].split("/")[0];
+				src = `https://player.bilibili.com/player.html?bvid=${BV}&page=${page}`;
+			}
+			//错误的url，不解析
+			else {
+				return innerContent;
+			}
+		}
+		//bili标签内输入错误内容，不解析
 		else {
 			return innerContent;
 		}
