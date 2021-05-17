c6d7cdb56417c88ce986b287c2825978565cb201
adddna <zzaadddna@live.com>
2021-05-18 00:25:20 +0800

    fix: CC98表情包作者

--- CC98.Forum/CC98.Forum/Components/UbbEditor/components/emoji.tsx
+++ CC98.Forum/CC98.Forum/Components/UbbEditor/components/emoji.tsx
@@ -133,7 +133,7 @@ export default class Emoji extends React.Component<EmojiProps> {
 
         //表情栏上的info
         const info = {
-            'CC98': <p className="ubb-emoji-info">该组表情由 <a target="_blank" href="/user/id/567066">洛予</a> 制作</p>,
+            'CC98': <p className="ubb-emoji-info">该组表情由 <a target="_blank" href="/user/id/567066">洛予</a> 和 <a target="_blank" href="/user/id/641262">大鲶鱼保佑</a> 制作</p>,
             'ac': <p className="ubb-emoji-info">该组表情由 <a target="_blank" href="http://www.acfun.cn">AcFun弹幕视频网</a> 提供</p>,
             'mj': <p className="ubb-emoji-info">该组表情由 <a target="_blank" href="//bbs.saraba1st.com/2b/forum.php">stage1st论坛</a> 提供</p>,
             'tb': <p className="ubb-emoji-info">该组表情由 <a target="_blank" href="//tieba.baidu.com ">百度贴吧</a> 提供</p>,
