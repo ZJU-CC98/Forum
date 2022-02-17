2d6e9a6d334a029a8b220ab37551797e4ad57ab9
97littleleaf11 <11172084+97littleleaf11@users.noreply.github.com>
2022-02-18 00:44:09 +0800

    添加风评统计

--- CC98.Forum/CC98.Forum/Components/Topic/Topic-Award.tsx
+++ CC98.Forum/CC98.Forum/Components/Topic/Topic-Award.tsx
@@ -7,13 +7,13 @@ interface Props {
     updateTime;
     awardInfo;
 }
-export class Award extends React.Component<Props, {info,shortInfo,count,showAll }>{
+export class Award extends React.Component<Props, { info, shortInfo, count, showAll, popularity_count, popularity_sum }>{
     constructor(props, content) {
         super(props, content);
         this.showAll = this.showAll.bind(this);
         this.hideAll = this.hideAll.bind(this);
         this.generateAwardInfo = this.generateAwardInfo.bind(this);
-        this.state = { info: [],shortInfo:[],count:0 ,showAll:false}
+        this.state = { info: [], shortInfo: [], count: 0, showAll: false, popularity_count: 0, popularity_sum: 0 }
     }
     hideAll() {
         this.setState({ showAll: false});
@@ -21,55 +21,47 @@ export class Award extends React.Component<Props, {info,shortInfo,count,showAll 
     showAll(){
         this.setState({ showAll: true });
     }
-    async componentDidMount() {
-        let shortInfo = [];
+    async update_award(first_ten = false) {
         let award = this.props.awardInfo;
         if (!award) { award = []; }
         const usersName = [];
-        let usersInfo = [];
         for (let i in award) {
             usersName[i] = award[i].operatorName;
         }
-       if (award.length>0)
-        usersInfo = await Utility.getUsersInfobyNames(usersName);
-      
-        for (let i in award) {
-            
+        let usersInfo = await Utility.getUsersInfobyNames(usersName);
+        let popularity_cnt = 0, popularity_sum = 0;
+        for (let i = 0; i < award.length; i++) {
             let thisUserInfo = Utility.getThisUserInfobyName(award[i].operatorName, usersInfo);
-            //console.log("find");
-            //console.log(thisUserInfo);
             award[i].portraitUrl = thisUserInfo.portraitUrl;
-            //console.log("award0");
-           // console.log(award[i]);
+            if (first_ten && i >= 10) break;
+            if (award[i].content.startsWith("风评值")) {
+                popularity_cnt += 1;
+                if (award[i].content.endsWith("+1")) {
+                    popularity_sum += 1;
+                } else {
+                    popularity_sum -= 1;
+                }
+            }
         }
-        if (award.length > 10) {
-            for (let i = 0; i < 10; i++) {
-                shortInfo[i] =this.generateAwardInfo(award[i]);
-             }
-        }
-        const info = award.map(this.generateAwardInfo.bind(this));
-        this.setState({ info: info,shortInfo:shortInfo,count:award.length})
-    }
-    async componentWillReceiveProps(newProps) {
+
         let shortInfo = [];
-        let award = newProps.awardInfo;
-        if (!award) { award = []; }
-        const usersName = [];
-        let usersInfo = [];
-        for (let i in award) {
-            usersName[i] = award[i].operatorName;
-        }
-        usersInfo = await Utility.getUsersInfobyNames(usersName);
         if (award.length > 10) {
             for (let i = 0; i < 10; i++) {
-                let thisUserInfo = Utility.getThisUserInfobyName(award[i].operatorName, usersInfo);
-                award[i].portraitUrl = thisUserInfo.portraitUrl;
-                console.log(award[i]);
                 shortInfo[i] = this.generateAwardInfo(award[i]);
             }
         }
+
         const info = award.map(this.generateAwardInfo.bind(this));
-        this.setState({ info: info, shortInfo: shortInfo, count: award.length });
+        this.setState({
+            info: info, shortInfo: shortInfo, count: award.length,
+            popularity_count: popularity_cnt, popularity_sum: popularity_sum,
+        })
+    }
+    async componentDidMount() {
+        await this.update_award(false);
+    }
+    async componentWillReceiveProps(newProps) {
+        await this.update_award(true);
     }
     
     generateAwardInfo(item) {
@@ -89,11 +81,15 @@ export class Award extends React.Component<Props, {info,shortInfo,count,showAll 
         if (this.state.info && this.state.info.length !== 0) {
             if (this.state.info.length < 10) {
                 $(awardInfoJQID).css("display", "");
-        } 
+            } 
         } else{
             $(awardInfoJQID).css("display", "none");
         }
         const btn = <button style={{ border: "#eaeaea solid thin", cursor:"pointer" }} onClick={this.state.showAll ? this.hideAll : this.showAll}>{this.state.showAll ? '收起' : '显示全部'}</button>;
+        const popularity_count = <div className="good tagSize">
+            <div>风评总数：{this.state.popularity_count}</div>
+            <div style={{ marginLeft: "3rem" }}>风评值：{this.state.popularity_sum}</div>
+        </div>;
         return <div className="column awardInfo" id={awardInfoID} >
             <div className="row" style={{ width: "25rem", fontSize: "0.8rem", marginBottom:"0.2rem" }}>
                 <div style={{ marginLeft:"3.1rem" }}>用户</div>
@@ -101,7 +97,8 @@ export class Award extends React.Component<Props, {info,shortInfo,count,showAll 
                 <div style={{ marginLeft: "8.9rem" }}>理由</div>
             </div>
             {this.state.count > 10 ? (this.state.showAll ? this.state.info : this.state.shortInfo) : this.state.info}
-            {this.state.count>10?btn:null}
+            {this.state.showAll ? popularity_count : null}
+            {this.state.count > 10 ? btn : null}
         </div>;
     }
 }
