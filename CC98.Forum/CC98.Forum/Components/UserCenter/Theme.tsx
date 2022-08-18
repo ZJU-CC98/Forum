89d6e332cdb03c71ae5e2a4bd22bf661c47f56b6
adddna <zzaadddna@live.com>
2022-08-18 16:55:54 +0800

    feat: 新秋季主题

--- CC98.Forum/CC98.Forum/Components/UserCenter/Theme.tsx
+++ CC98.Forum/CC98.Forum/Components/UserCenter/Theme.tsx
@@ -5,7 +5,7 @@ import { UserInfo } from '../../States/AppState';
 import { connect } from 'react-redux';
 import { RootState } from '../../Store';
 
-const themeList = ['系统默认', '冬季', '春季（浅色）', '春季（深色）', '夏季', '秋季（橙色）', '秋季（红色）', '双十一', '中秋（暗）', '中秋（亮）', '小雪（暗）', '小雪（亮）', '春节（暗）', '春节（亮）', '仲春', '端午', '清明'];
+const themeList = ['系统默认', '冬季', '春季（浅）', '春季（深）', '夏季', '秋季（暗）', '秋季（亮）', '双十一', '中秋（暗）', '中秋（亮）', '小雪（暗）', '小雪（亮）', '春节（暗）', '春节（亮）', '仲春', '端午', '清明'];
 
 interface Props {
   userInfo: UserInfo;
@@ -38,13 +38,13 @@ class Theme extends React.Component<Props> {
         <div className="user-theme-config">
           <button style={{ backgroundColor: '#ffffff', border: "solid 1px" }} key={0} onClick={() => this.handleSubmit(0)} disabled={this.props.userInfo.theme === 0}>系统默认</button>
           <button style={{ backgroundColor: '#79b8ca' }} key={1} onClick={() => this.handleSubmit(1)} disabled={this.props.userInfo.theme === 1}>冬季</button>
-          <button style={{ backgroundColor: '#b1d396' }} key={2} onClick={() => this.handleSubmit(2)} disabled={this.props.userInfo.theme === 2}>春季（浅色）</button>
-          <button style={{ backgroundColor: '#95b675' }} key={3} onClick={() => this.handleSubmit(3)} disabled={this.props.userInfo.theme === 3}>春季（深色）</button>
+          <button style={{ backgroundColor: '#b1d396' }} key={2} onClick={() => this.handleSubmit(2)} disabled={this.props.userInfo.theme === 2}>春季（浅）</button>
+          <button style={{ backgroundColor: '#95b675' }} key={3} onClick={() => this.handleSubmit(3)} disabled={this.props.userInfo.theme === 3}>春季（深）</button>
           <button style={{ backgroundColor: '#5198d8' }} key={4} onClick={() => this.handleSubmit(4)} disabled={this.props.userInfo.theme === 4}>夏季</button>
         </div>
         <div className="user-theme-config">
-          <button style={{ backgroundColor: '#F4A460' }} key={5} onClick={() => this.handleSubmit(5)} disabled={this.props.userInfo.theme === 5}>秋季（橙色）</button>
-          <button style={{ backgroundColor: '#B22222' }} key={6} onClick={() => this.handleSubmit(6)} disabled={this.props.userInfo.theme === 6}>秋季（红色）</button>
+          <button style={{ backgroundColor: 'rgb(183, 115, 65)' }} key={5} onClick={() => this.handleSubmit(5)} disabled={this.props.userInfo.theme === 5}>秋季（暗）</button>
+          <button style={{ backgroundColor: 'rgb(235, 142, 85)' }} key={6} onClick={() => this.handleSubmit(6)} disabled={this.props.userInfo.theme === 6}>秋季（亮）</button>
           <button style={{ backgroundColor: 'rgb(240,125,145)' }} key={7} onClick={() => this.handleSubmit(7)} disabled={this.props.userInfo.theme === 7}>双十一交友</button>
           <button style={{ backgroundColor: 'rgb(38,118,129)' }} key={8} onClick={() => this.handleSubmit(8)} disabled={this.props.userInfo.theme === 8}>中秋（暗）</button>
           <button style={{ backgroundColor: 'rgb(52,150,159)' }} key={9} onClick={() => this.handleSubmit(9)} disabled={this.props.userInfo.theme === 9}>中秋（亮）</button>
