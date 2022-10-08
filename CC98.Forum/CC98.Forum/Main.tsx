4eb1c648b5a1edaeb2debfbba1209becd8cdc02c
Ze-Zheng Wu <zzwu@zju.edu.cn>
2022-10-08 20:58:33 +0800

    feat: add spatial watermark to pages

--- CC98.Forum/CC98.Forum/Main.tsx
+++ CC98.Forum/CC98.Forum/Main.tsx
@@ -1,22 +1,23 @@
-﻿import 'es6-promise/auto';
-import 'core-js/shim';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as $ from 'jquery';
-import store from './Store';
-import { Provider } from 'react-redux';
-import 'whatwg-fetch';
-import 'blueimp-canvas-to-blob';
+﻿import "es6-promise/auto";
+import "core-js/shim";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as $ from "jquery";
+import store from "./Store";
+import { Provider } from "react-redux";
+import "whatwg-fetch";
+import "blueimp-canvas-to-blob";
+import { initWatermark } from "spatial-watermark";
 
-import { Constants } from './Components/Constant';
-import App from './Components/App';
-import ErrorBoundary from './Components/ErrorBoundary';
-import { IndexedDB } from './IndexedDB/IndexedDB';
-import { shouldUseIndexedDb } from './config';
-import zh_CN from 'antd/lib/locale-provider/zh_CN';
-import { LocaleProvider } from 'antd';
-import moment from 'moment'
-moment.locale('zh-cn')
+import { Constants } from "./Components/Constant";
+import App from "./Components/App";
+import ErrorBoundary from "./Components/ErrorBoundary";
+import { IndexedDB } from "./IndexedDB/IndexedDB";
+import { shouldUseIndexedDb } from "./config";
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+import { LocaleProvider } from "antd";
+import moment from "moment";
+moment.locale("zh-cn");
 /**
  * 项目初始化代码
  */
@@ -26,7 +27,7 @@ async function initialize() {
 
   // 输出一些没用的东西
   // console.info('%c       ', 'font-size: 100px; background: url(http://cdn.nyanit.com/nyan2.gif) no-repeat;');
-  console.info('%cCC98 Durian', 'font-size: 80px; fontFamily: Big');
+  console.info("%cCC98 Durian", "font-size: 80px; fontFamily: Big");
 
   // 显示应用程序核心内容
   ReactDOM.render(
@@ -37,11 +38,14 @@ async function initialize() {
         </LocaleProvider>
       </Provider>
     </ErrorBoundary>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 
-  if (process.env.NODE_ENV === 'development') {
-    const { whyDidYouUpdate } = require('why-did-you-update');
+  // 添加水印
+  initWatermark();
+
+  if (process.env.NODE_ENV === "development") {
+    const { whyDidYouUpdate } = require("why-did-you-update");
     //	whyDidYouUpdate(React);
   }
 }
