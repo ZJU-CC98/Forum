af93a0b5d95d91e08c518e55735f7224113e76c8
Ze-Zheng Wu <zzwu@zju.edu.cn>
2022-10-12 12:40:02 +0800

    fix: add dplayer and aplayer css source map

--- CC98.Forum/vscodeConfig/webpack.config.vscode.js
+++ CC98.Forum/vscodeConfig/webpack.config.vscode.js
@@ -106,7 +106,9 @@ module.exports = {
             { from: 'node_modules/font-awesome', to: 'static/content/font-awesome/' },
             { from: 'node_modules/spectrum-colorpicker/spectrum.js', to: 'static/scripts/lib/spectrum/spectrum.js' },
             { from: 'node_modules/dplayer/dist/DPlayer.min.css', to: 'static/content/DPlayer.min.css' },
+            { from: 'node_modules/dplayer/dist/DPlayer.min.css.map', to: 'static/content/DPlayer.min.css.map' },
             { from: 'node_modules/aplayer/dist/APlayer.min.css', to: 'static/content/APlayer.min.css' },
+            { from: 'node_modules/aplayer/dist/APlayer.min.css.map', to: 'static/content/APlayer.min.css.map' },
             { from: 'node_modules/hls.js/dist/hls.min.js', to: 'static/content/hls.min.js' },
         ]),
 
