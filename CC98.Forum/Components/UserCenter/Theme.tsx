42986c8be1ef61112c7fd58afdc02da78c493c65
Iris Sakura <sgjsakura@hotmail.com>
2022-11-30 15:09:18 +0800

    Update

--- CC98.Forum/Components/UserCenter/Theme.tsx
+++ CC98.Forum/Components/UserCenter/Theme.tsx
@@ -5,10 +5,17 @@ import { UserInfo } from '../../States/AppState';
 import { connect } from 'react-redux';
 import { RootState } from '../../Store';
 
-//const themeList = ['系统默认', '冬季', '春季（浅）', '春季（深）', '夏季', '秋季（橙）', '秋季（红）', '双十一', '中秋（暗）', '中秋（亮）', '小雪（暗）', '小雪（亮）', '春节（暗）', '春节（亮）', '仲春', '端午', '清明', '秋色之空（暗）', '秋色之空（亮）', '冬日暖雪（暗）', '冬日暖雪（亮）'];
-
+/** 
+ * 该组件需要使用的属性列表。
+ */
 interface Props {
+  /**
+   * 当前界面关联的用户信息。
+   */
   userInfo: UserInfo;
+  /**
+   * 修改用户界面操作执行的回调。
+   */
   changeUserInfo: (userInfo: UserInfo) => void;
 }
 
@@ -83,32 +90,37 @@ class Theme extends React.Component<Props> {
     return (
       <div className="user-theme">
         <div className="user-theme-info"><h2>切换皮肤</h2><p>当前皮肤：{themeList[this.props.userInfo.theme]}</p></div>
-        <div className="user-theme-config">
-          {/** 
-          <button style={{ backgroundColor: '#ffffff', border: "solid 1px" }} key={0} onClick={() => this.handleSubmit(0)} disabled={this.props.userInfo.theme === 0}>系统默认</button>
-          <button style={{ backgroundColor: '#79b8ca' }} key={1} onClick={() => this.handleSubmit(1)} disabled={this.props.userInfo.theme === 1}>冬季</button>
-          <button style={{ backgroundColor: '#b1d396' }} key={2} onClick={() => this.handleSubmit(2)} disabled={this.props.userInfo.theme === 2}>春季（浅）</button>
-          <button style={{ backgroundColor: '#95b675' }} key={3} onClick={() => this.handleSubmit(3)} disabled={this.props.userInfo.theme === 3}>春季（深）</button>
-          <button style={{ backgroundColor: '#5198d8', color: "white" }} key={4} onClick={() => this.handleSubmit(4)} disabled={this.props.userInfo.theme === 4}>夏季</button>
-          <button style={{ backgroundColor: '#F4A460' }} key={5} onClick={() => this.handleSubmit(5)} disabled={this.props.userInfo.theme === 5}>秋季（橙）</button>
-          <button style={{ backgroundColor: '#B22222', color: "white" }} key={6} onClick={() => this.handleSubmit(6)} disabled={this.props.userInfo.theme === 6}>秋季（红）</button>
-          <button style={{ backgroundColor: 'rgb(240,125,145)' }} key={7} onClick={() => this.handleSubmit(7)} disabled={this.props.userInfo.theme === 7}>双十一交友</button>
-          <button style={{ backgroundColor: 'rgb(38,118,129)', color: "white" }} key={8} onClick={() => this.handleSubmit(8)} disabled={this.props.userInfo.theme === 8}>中秋（暗）</button>
-          <button style={{ backgroundColor: 'rgb(52,150,159)', color: "white" }} key={9} onClick={() => this.handleSubmit(9)} disabled={this.props.userInfo.theme === 9}>中秋（亮）</button>
-          <button style={{ backgroundColor: 'rgb(3, 57, 117)', color: "white" }} key={10} onClick={() => this.handleSubmit(10)} disabled={this.props.userInfo.theme === 10}>小雪（暗）</button>
-          <button style={{ backgroundColor: 'rgb(122, 146, 194)', color: "white" }} key={11} onClick={() => this.handleSubmit(11)} disabled={this.props.userInfo.theme === 11}>小雪（亮）</button>
-          <button style={{ backgroundColor: 'rgb(205,0,0)', color: "white" }} key={12} onClick={() => this.handleSubmit(12)} disabled={this.props.userInfo.theme === 12}>春节（暗）</button>
-          <button style={{ backgroundColor: 'rgb(214,14,36)', color: "white" }} key={13} onClick={() => this.handleSubmit(13)} disabled={this.props.userInfo.theme === 13}>春节（亮）</button>
-          <button style={{ backgroundColor: 'rgb(70, 141, 57)', color: "white" }} key={14} onClick={() => this.handleSubmit(14)} disabled={this.props.userInfo.theme === 14}>仲春</button>
-          <button style={{ backgroundColor: 'rgb(53, 120, 188)', color: "white" }} key={15} onClick={() => this.handleSubmit(15)} disabled={this.props.userInfo.theme === 15}>端午</button>
-          <button style={{ backgroundColor: 'rgb(106, 132, 113)', color: "white" }} key={16} onClick={() => this.handleSubmit(16)} disabled={this.props.userInfo.theme === 16}>清明</button>
-          <button style={{ backgroundColor: 'rgb(183, 115, 65)', color: "white" }} key={17} onClick={() => this.handleSubmit(17)} disabled={this.props.userInfo.theme === 17}>秋色之空（暗）</button>
-          <button style={{ backgroundColor: 'rgb(235, 142, 85)', color: "white" }} key={18} onClick={() => this.handleSubmit(18)} disabled={this.props.userInfo.theme === 18}>秋色之空（亮）</button>
-          <button style={{ backgroundColor: 'rgb(125,85,119)', color: "white" }} key={19} onClick={() => this.handleSubmit(19)} disabled={this.props.userInfo.theme === 19}>冬日暖雪（暗）</button>
-          <button style={{ backgroundColor: 'rgb(181, 127, 163)', color: "white" }} key={20} onClick={() => this.handleSubmit(20)} disabled={this.props.userInfo.theme === 20}>冬日暖雪（亮）</button>
-        */}
-          {buttonStyles.map(this.generateButton)}
 
+        <div>
+
+          <div>
+            <label>
+              <input type="checkbox" /> 启用皮肤昼夜更换功能
+            </label>
+            <p className="help-text">只有通知提供暗色和亮色的主题才支持昼夜更换功能。如果你选择其他皮肤，这个选项将不会产生任何效果。</p>
+
+            <div>
+              <label htmlFor="">日间开始时刻</label>
+              <input type="time" />
+              
+              <label htmlFor="">夜间开始时刻</label>
+              <input type="time" />
+            </div>
+            
+            <div>
+              <label>
+                <input type="checkbox" /> 和浏览器的日夜模式自动同步
+              </label>
+            </div>
+            <p className="help-text">注意：你的浏览器必须向网站提供是否使用日夜模式的相关信息，这个设置才会生效。目前，并不是所有浏览器都具有这项功能。浏览器不支持时，将会使用上面设置的日夜开始时刻更换皮肤。</p>
+           
+
+          </div>
+          
+        </div>
+
+        <div className="user-theme-config">        
+          {buttonStyles.map(this.generateButton)}
         </div>
       </div>
     );
