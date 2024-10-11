2aeda9668736d32cd7de7a1edc7cf5a32921d548
tsukiko <tsukiko@outlook.com>
2024-10-11 23:07:11 +0800

    fix: trace

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -9,7 +9,7 @@ export class Footer extends React.Component<{}, AppState> {   //底部
                     <div style={{ marginRight: "15px", color: "#000" }}>CC98运营管理团队</div>
                     <a href="https://www.cc98.org" target="_blank">CC98论坛</a>|
                     <a href="https://space.bilibili.com/222288454" target="_blank">bilibili官号</a>|
-                    <a href="http://www.nexushd.org" target="_blank">NexusHD高清社区</a>|
+                    <a href="https://www.nexushd.org" target="_blank">NexusHD高清社区</a>|
                     <a href="/user/id/517471" target="_blank">雕塑流年</a>|
                     <a href="https://space.bilibili.com/695376760" target="_blank">CC98舞团</a>|
                     <a href="https://gaming.cc98.org" target="_blank">CC98竞猜中心</a>|
@@ -23,7 +23,7 @@ export class Footer extends React.Component<{}, AppState> {   //底部
                     <a href="http://xcb.zju.edu.cn" target="_blank">浙江大学党委宣传部</a>|
                     <a href="http://www.cs.zju.edu.cn" target="_blank">浙江大学计算机学院</a>|
                     <a href="https://zjuers.com" target="_blank">ZJUers轻首页</a>|
-                    <a href="http://www.qsc.zju.edu.cn" target="_blank">求是潮</a>|
+                    <a href="https://www.qsc.zju.edu.cn" target="_blank">求是潮</a>|
                     <a href="http://www.zjulib.com" target="_blank">浙大图助</a>|
                     <a href="https://www.zdgd.zju.edu.cn" target="_blank" style={{ color: "#787878" }}>浙江大学广播电视网</a>|
                     <a href="http://www.zju88.org/agent/index.do" target="_blank" style={{ color: "#787878" }}>飘渺水云间</a>|
