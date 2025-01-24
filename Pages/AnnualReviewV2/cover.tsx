f4849d9161dae4e39b5cb1d08093aea6c00b39ce
tsukiko <tsukiko@outlook.com>
2025-01-24 14:40:56 +0800

    update filename (#112)

--- Pages/AnnualReviewV2/cover.tsx
+++ Pages/AnnualReviewV2/cover.tsx
@@ -1,2 +1,2 @@
 import React from 'react'
-export default () => <div className="annual-review-page" style={{backgroundImage: `url('/static/images/annual-review/cover.jpg')`}}></div>
+export default () => <div className="annual-review-page" style={{ backgroundImage: `url('/static/images/annual-review/cover2024.jpg')` }}></div>
